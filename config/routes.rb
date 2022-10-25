2d3dd2e5d29b86735ed6144a0d25fb3777030cad
Tobias Vetter <tobias.vetter@mailbox.org>
2022-10-25 17:57:46 +0200

    Removing not needed comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   Rails.application.routes.draw do
     root "spa#index"
   end
