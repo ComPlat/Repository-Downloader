ad12f6e0aba6ec70129dd1b785ba4dc1b9ae2ea8
Christian Deme <christian.deme@cleanercode.de>
2022-10-25 16:47:41 +0200

    Adding new line at EOF, moving short functions into one line arrow function

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-function App() {
-    return (<h1>Hello World!</h1>);
-}
+const App = () => <h1>Hello World!</h1>
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('spa'),
-);
+ReactDOM.render(<App/>, document.getElementById('spa'),);
