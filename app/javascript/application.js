b87762520fe114d41012f439a8f0784362551a0b
Tobias Vetter <tobias.vetter@mailbox.org>
2022-10-26 12:02:43 +0200

    Removing not needed declarations file, removing not needed transpiled js file (that caused asset pipeline to crash), deactivating TypeScript Service in RubyMine config that caused generation of .js files that crash asset pipeline

--- app/javascript/application.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-const react_dom_1 = __importDefault(require("react-dom"));
-const App = () => react_1.default.createElement("h1", null, "Hello World!");
-react_dom_1.default.render(react_1.default.createElement(App, null), document.getElementById('spa'));
