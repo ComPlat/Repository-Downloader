0e6ce20cfd749ca872b03e32ea4d329675b74f47
Tobias Vetter <tobias.vetter@mailbox.org>
2022-10-26 16:01:51 +0200

    Implementing Capybara config to work in containerized ENV

---
+++ spec/support/capybara.rb
@@ -0,0 +1,19 @@
+HUB_URL = ENV["HUB_URL"].freeze
+
+return unless HUB_URL
+
+OPTS = {"goog:chromeOptions": {args: %w[headless disable-gpu window-size=1400x1400]}}.freeze
+
+Capybara.register_driver :headless_selenium_chrome_in_container do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome OPTS
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: HUB_URL, capabilities:)
+end
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    Capybara.server_host = "0.0.0.0"
+    Capybara.server_port = 4000
+    Capybara.app_host = "http://web:4000"
+    driven_by(:headless_selenium_chrome_in_container)
+  end
+end
