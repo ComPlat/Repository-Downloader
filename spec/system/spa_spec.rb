5fdd3db22526ec9193f8b43686bf471ada33d717
Tobias Vetter <tobias.vetter@mailbox.org>
2022-10-27 12:09:35 +0200

    Fixing test spec failure again.

--- spec/system/spa_spec.rb
+++ spec/system/spa_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "SPA" do
   it do
     visit "/"
-    expect(page).to have_selector "h1", text: "Hello World! BROKEN ON PURPOSE TO TEST PIPELINE"
+    expect(page).to have_selector "h1", text: "Hello World!"
   end
 end
