44ed9499f0f3a9787846d2fbfcd2ea7f40696937
Tobias Vetter <tobias.vetter@mailbox.org>
2022-10-28 09:53:13 +0200

    ESLint for JS like files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    './app/javascript/**/*.js',
+  ],
+};
