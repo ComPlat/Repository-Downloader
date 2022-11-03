48e04214d61bf1a66ec2326cc88215524cb5090d
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-03 18:08:43 +0100

    Change specs to use factories, add describe blocks for each method, use heredocs for expected formats, use string interpolation for values in heredocs

--- spec/mappers/person_mapper_spec.rb
+++ spec/mappers/person_mapper_spec.rb
@@ -2,15 +2,56 @@ describe PersonMapper do
   context "when called without any arguments" do
     let(:person_mapper) { build :person_mapper }
 
+    let(:expected_nil_return_value) { "null" }
+
     it { expect(person_mapper).to be_a described_class }
     it { expect(person_mapper.first_name).to be_nil }
     it { expect(person_mapper.last_name).to be_nil }
     it { expect(person_mapper.age).to be_nil }
     it { expect(person_mapper.married).to be false }
     it { expect(person_mapper.hobbies).to eq [] }
-    it { expect(person_mapper.to_json).to eq "{\"first_name\":null,\"last_name\":null,\"age\":null,\"married\":false,\"hobbies\":[],\"address\":null}" }
-    it { expect(person_mapper.to_xml).to eq "<person><first_name/><last_name/><age/><married>false</married><address/></person>" }
-    it { expect(person_mapper.to_csv).to eq ",,,false,[],\n" }
+
+    describe "#to_json" do
+      let(:expected_json) do
+        <<~JSON.strip
+          {\
+          "first_name\":#{expected_nil_return_value},\
+          "last_name\":#{expected_nil_return_value},\
+          "age\":#{expected_nil_return_value},\
+          "married\":false,\
+          "hobbies\":[],\
+          "address\":#{expected_nil_return_value}}
+        JSON
+      end
+
+      it { expect(person_mapper.to_json).to eq expected_json }
+    end
+
+    describe "#to_xml" do
+      let(:expected_xml) do
+        <<~XML.strip
+          <person>\
+          <first_name/>\
+          <last_name/>\
+          <age/>\
+          <married>false</married>\
+          <address/>\
+          </person>
+        XML
+      end
+
+      it { expect(person_mapper.to_xml).to eq expected_xml }
+    end
+
+    describe "#to_csv" do
+      let(:expected_csv) do
+        <<~CSV.strip
+          ,,,false,[],\n
+        CSV
+      end
+
+      it { expect(person_mapper.to_csv.strip).to eq expected_csv }
+    end
   end
 
   context "when called with all arguments" do
@@ -27,85 +68,133 @@ describe PersonMapper do
     it { expect(person_mapper.address.street).to eq args[:address].street }
     it { expect(person_mapper.address.zip).to eq args[:address].zip }
 
-    # TODO: Look to address_mapper_spec.rb as example.
-    #       Add describe block around each it block for each method tested.
-    #       Move very long eq string to let block within this describe block.
-    #       Use string interpolation for values in key-value like data-structures that are represented.
-    #       Use Heredoc instead of lengthy strings.
-    #       After that Tobias and Rubocop will be happy.
-    it do
-      expect(person_mapper.to_json)
-        .to eq "{\"first_name\":\"John\"," \
-                "\"last_name\":\"Doe\"," \
-                "\"age\":25," \
-                "\"married\":true," \
-                "\"hobbies\":[\"Guitar\",\"Music\",\"Reading\"]," \
-                "\"address\":{\"city\":\"city\",\"street\":\"street\",\"zip\":\"zip\"}}"
+    describe "#to_json" do
+      let(:expected_json) do
+        <<~JSON.strip
+          {\
+          "first_name":"#{args[:first_name]}",\
+          "last_name":"#{args[:last_name]}",\
+          "age":#{args[:age]},\
+          "married":#{args[:married]},\
+          "hobbies":["Guitar","Music","Reading"],\
+          "address":{\
+          "city":"#{args[:address].city}",\
+          "street":"#{args[:address].street}",\
+          "zip":"#{args[:address].zip}"\
+          }\
+          }
+        JSON
+      end
+
+      it { expect(person_mapper.to_json).to eq expected_json }
     end
 
-    it do
-      expect(person_mapper.to_xml)
-        .to eq "<person>" \
-                  "<first_name>John</first_name>" \
-                  "<last_name>Doe</last_name>" \
-                  "<age>25</age>" \
-                  "<married>true</married>" \
-                  "<hobbies>Guitar</hobbies>" \
-                  "<hobbies>Music</hobbies>" \
-                  "<hobbies>Reading</hobbies>" \
-                  "<address>" \
-                    "<city>city</city>" \
-                    "<street>street</street>" \
-                    "<zip>zip</zip>" \
-                  "</address>" \
-                "</person>"
+    describe "#to_xml" do
+      let(:expected_xml) do
+        <<~XML.strip
+          <person>\
+          <first_name>#{args[:first_name]}</first_name>\
+          <last_name>#{args[:last_name]}</last_name>\
+          <age>#{args[:age]}</age>\
+          <married>#{args[:married]}</married>\
+          <hobbies>Guitar</hobbies>\
+          <hobbies>Music</hobbies>\
+          <hobbies>Reading</hobbies>\
+          <address>\
+          <city>#{args[:address].city}</city>\
+          <street>#{args[:address].street}</street>\
+          <zip>#{args[:address].zip}</zip>\
+          </address>\
+          </person>
+        XML
+      end
+
+      it { expect(person_mapper.to_xml).to eq expected_xml }
     end
 
-    it do
-      expect(person_mapper.to_csv)
-        .to eq "John,Doe,25,true,\"[\"\"Guitar\"\", \"\"Music\"\", \"\"Reading\"\"]\"," \
-                "\"{\"\"city\"\"=>\"\"city\"\", \"\"street\"\"=>\"\"street\"\", \"\"zip\"\"=>\"\"zip\"\"}\"\n"
+    describe "#to_csv" do
+      let(:expected_csv) do
+        <<~CSV.strip
+          #{args[:first_name]},#{args[:last_name]},#{args[:age]},#{args[:married]},\
+          \"[\"\"Guitar\"\", \"\"Music\"\", \"\"Reading\"\"]\",\
+          \"{\"\"city\"\"=>\"\"#{args[:address].city}"\",\
+           \"\"street\"\"=>\"\"#{args[:address].street}"\",\
+           \"\"zip\"\"=>\"\"#{args[:address].zip}"\"}\"
+        CSV
+      end
+
+      it { expect(person_mapper.to_csv.strip).to eq expected_csv }
     end
   end
 
   context "when called some arguments" do
-    let(:person_mapper) { described_class.new first_name:, last_name: nil, age:, hobbies:, address: }
+    let(:args) { attributes_for :person_mapper, :with_all_args, last_name: nil, married: false }
+    let(:person_mapper) { described_class.new(**args) }
+    let(:expected_nil_return_value) { "null" }
 
     it { expect(person_mapper).to be_a described_class }
-    it { expect(person_mapper.first_name).to eq first_name }
+    it { expect(person_mapper.first_name).to eq args[:first_name] }
     it { expect(person_mapper.last_name).to be_nil }
-    it { expect(person_mapper.age).to eq age }
+    it { expect(person_mapper.age).to eq args[:age] }
     it { expect(person_mapper.married).to be false }
 
-    it do
-      expect(person_mapper.to_json)
-        .to eq "{\"first_name\":\"John\",\"last_name\":null,\"age\":25,\"married\":false," \
-                "\"hobbies\":[\"Guitar\",\"Music\",\"Reading\"]," \
-                "\"address\":{\"city\":\"city\",\"street\":\"street\",\"zip\":\"zip\"}}"
+    describe "#to_json" do
+      let(:expected_json) do
+        <<~JSON.strip
+          {\
+          "first_name\":\"#{args[:first_name]}",\
+          "last_name\":#{expected_nil_return_value},\
+          "age\":#{args[:age]},\
+          "married\":#{args[:married]},\
+          "hobbies\":[\
+          "Guitar\",\
+          "Music\",\
+          "Reading\"],\
+          "address\":{\
+          "city\":\"#{args[:address].city}",\
+          "street\":\"#{args[:address].street}",\
+          "zip\":\"#{args[:address].zip}"}}
+        JSON
+      end
+
+      it { expect(person_mapper.to_json).to eq expected_json }
     end
 
-    it do
-      expect(person_mapper.to_xml)
-        .to eq "<person>" \
-                  "<first_name>John</first_name>" \
-                  "<last_name/>" \
-                  "<age>25</age>" \
-                  "<married>false</married>" \
-                  "<hobbies>Guitar</hobbies>" \
-                  "<hobbies>Music</hobbies>" \
-                  "<hobbies>Reading</hobbies>" \
-                  "<address>" \
-                    "<city>city</city>" \
-                    "<street>street</street>" \
-                    "<zip>zip</zip>" \
-                  "</address>" \
-                "</person>"
+    describe "#to_xml" do
+      let(:expected_xml) do
+        <<~XML.strip
+          <person>\
+          <first_name>#{args[:first_name]}</first_name>\
+          <last_name/>\
+          <age>#{args[:age]}</age>\
+          <married>#{args[:married]}</married>\
+          <hobbies>Guitar</hobbies>\
+          <hobbies>Music</hobbies>\
+          <hobbies>Reading</hobbies>\
+          <address>\
+          <city>#{args[:address].city}</city>\
+          <street>#{args[:address].street}</street>\
+          <zip>#{args[:address].zip}</zip>\
+          </address>\
+          </person>
+        XML
+      end
+
+      it { expect(person_mapper.to_xml).to eq expected_xml }
     end
 
-    it do
-      expect(person_mapper.to_csv)
-        .to eq "John,,25,false,\"[\"\"Guitar\"\", \"\"Music\"\", \"\"Reading\"\"]\"," \
-               "\"{\"\"city\"\"=>\"\"city\"\", \"\"street\"\"=>\"\"street\"\", \"\"zip\"\"=>\"\"zip\"\"}\"\n"
+    describe "#to_csv" do
+      let(:expected_csv) do
+        <<~CSV.strip
+          #{args[:first_name]},#{args[:last_name]},#{args[:age]},#{args[:married]},\
+          \"[\"\"Guitar\"\", \"\"Music\"\", \"\"Reading\"\"]\",\
+          \"{\"\"city\"\"=>\"\"#{args[:address].city}"\",\
+           \"\"street\"\"=>\"\"#{args[:address].street}"\",\
+           \"\"zip\"\"=>\"\"#{args[:address].zip}"\"}\"\n
+        CSV
+      end
+
+      it { expect(person_mapper.to_csv.strip).to eq expected_csv }
     end
   end
 end
