817742f69bf23f3f2319f12a59c61b22f9ca5b53
Christian Deme <christian.deme@cleanercode.de>
2022-11-03 18:07:02 +0100

    Add lines to fix and avoid CORS error

--- app/api/api/base.rb
+++ app/api/api/base.rb
@@ -2,6 +2,13 @@ module API
   class Base < Grape::API
     format :json
 
+    # HINT: Needed to avoid CORS (Cross-Origin Resource Sharing) error.
+    #       Swagger UI returns response code 0 if these lines aren't here.
+    before do
+      header["Access-Control-Allow-Origin"] = "*"
+      header["Access-Control-Request-Method"] = "*"
+    end
+
     mount API::Messages
     add_swagger_documentation
   end
