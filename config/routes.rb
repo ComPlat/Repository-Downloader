50853e2db999548fe0d47376a0654c7ccd422b15
Christian Deme <christian.deme@cleanercode.de>
2022-11-03 12:08:07 +0100

    Revert not intended indentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   root "spa#index"
   mount API::Base => "/api"
   mount GrapeSwaggerRails::Engine => "/swagger"
