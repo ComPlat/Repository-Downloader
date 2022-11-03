28fe1f2f7064a5f7cfbc0789b7e114d58cac06e7
Christian Deme <christian.deme@cleanercode.de>
2022-11-03 11:31:16 +0100

    Remove artefacts in base.rb

--- app/api/api/base.rb
+++ app/api/api/base.rb
@@ -1,13 +1,6 @@
-require "grape-swagger"
-
 module API
   class Base < Grape::API
     format :json
-
-    before do
-      header["Access-Control-Allow-Origin"] = "*"
-      header["Access-Control-Request-Method"] = "*"
-    end
 
     mount API::Messages
     add_swagger_documentation
