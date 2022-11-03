b05994b1899962058f5863275cd3ba04785fcbaf
Christian Deme <christian.deme@cleanercode.de>
2022-11-03 11:53:52 +0100

    Remove artefact files, remove not needed methods in messages.rb

--- app/api/api/messages.rb
+++ app/api/api/messages.rb
@@ -11,35 +11,12 @@ module API
       params do
         requires :id, type: Integer, desc: "Message ID"
       end
-      get do
-        Message.find(params[:id])
-      end
-
-      desc "Create a message"
-      params do
-        requires :title, type: String, desc: "Message title"
-        requires :content, type: String, desc: "Message content"
-      end
-      post do
-        Message.create(title: params[:title], content: params[:content])
-      end
-
-      desc "Update a message"
-      params do
-        requires :id, type: String, desc: "Message ID"
-        requires :title, type: String, desc: "Message title"
-        requires :content, type: String, desc: "Message content"
-      end
-      post do
-        Message.find(params[:id]).update(title: params[:title], content: params[:content])
-      end
-
-      desc "Delete a message"
-      params do
-        requires :id, type: Integer, desc: "Message ID"
-      end
-      delete ":id" do
-        Message.find(params[:id]).destroy
+      route_param :id, type: Integer do
+        get do
+          Message.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          error!("Message not found", 404)
+        end
       end
     end
   end
