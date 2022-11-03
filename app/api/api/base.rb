2112377f6aad033fbfbc9695520ff530a897050d
Christian Deme <christian.deme@cleanercode.de>
2022-11-03 18:18:10 +0100

    Add support for XML content type

--- app/api/api/base.rb
+++ app/api/api/base.rb
@@ -1,6 +1,7 @@
 module API
   class Base < Grape::API
-    format :json
+    content_type :json, "application/json"
+    content_type :xml, "application/xml"
 
     # HINT: Needed to avoid CORS (Cross-Origin Resource Sharing) error.
     #       Swagger UI returns response code 0 if these lines aren't here.
