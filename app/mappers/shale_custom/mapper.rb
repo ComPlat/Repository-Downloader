85a363ff4698a8aae5875f1bbb5cc16d3d1fd608
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-03 22:38:39 +0100

    fixed complex bug with inheritance/ singleton

--- app/mappers/shale_custom/mapper.rb
+++ app/mappers/shale_custom/mapper.rb
@@ -4,8 +4,9 @@ module ShaleCustom
     @csv_mapping = Shale::Mapping::Dict.new(render_nil_default: true)
     @xml_mapping = ShaleCustom::Xml.new
 
-    def self.inherited(subclass)
-      super.root subclass.to_s.gsub("Mapper", "").downcase
+    def initialize(**args)
+      super(**args)
+      self.class.instance_variable_get(:@xml_mapping).root self.class.name.gsub("Mapper", "").downcase
     end
   end
 end
--- spec/mappers/shale_custom/mapper_spec.rb
+++ spec/mappers/shale_custom/mapper_spec.rb
@@ -5,5 +5,5 @@ describe ShaleCustom::Mapper do
   it { expect(described_class.csv_mapping.instance_variable_get(:@render_nil_default)).to be true }
   it { expect(described_class.json_mapping).to be_a Shale::Mapping::Dict }
   it { expect(described_class.json_mapping.instance_variable_get(:@render_nil_default)).to be true }
-  it { expect(described_class.inherited(AddressMapper)).to eq "address" }
+  it { expect(AddressMapper.new.class.instance_variable_get(:@xml_mapping).instance_variable_get(:@root)).to eq "address" }
 end
