d732e6ffc8d6fc129d39d498477f7bc4dcef3bf1
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-07 11:15:10 +0100

    Add 3 specs for .to_csv

--- spec/mappers/shale_custom/mapper_spec.rb
+++ spec/mappers/shale_custom/mapper_spec.rb
@@ -6,4 +6,40 @@ describe ShaleCustom::Mapper do
   it { expect(described_class.json_mapping).to be_a Shale::Mapping::Dict }
   it { expect(described_class.json_mapping.instance_variable_get(:@render_nil_default)).to be true }
   it { expect(AddressMapper.new.class.instance_variable_get(:@xml_mapping).instance_variable_get(:@root)).to eq "address" }
+
+  describe ".to_csv" do
+    let(:address_mapper) { build :address_mapper }
+
+    context "with default parameters headers are enabled" do
+      let(:expected_csv) do
+        <<~CSV
+          city,street,zip
+          ,,
+        CSV
+      end
+
+      it { expect(address_mapper.to_csv).to eq_without_whitespace expected_csv }
+    end
+
+    context "when parameters are overridden" do
+      let(:expected_csv) do
+        <<~CSV
+          ,,
+        CSV
+      end
+
+      it { expect(address_mapper.to_csv(headers: false)).to eq_without_whitespace expected_csv }
+    end
+
+    context "when additional parameters are given" do
+      let(:expected_csv) do
+        <<~CSV
+          city|street|zip
+          ||
+        CSV
+      end
+
+      it { expect(address_mapper.to_csv(col_sep: "|")).to eq_without_whitespace expected_csv }
+    end
+  end
 end
