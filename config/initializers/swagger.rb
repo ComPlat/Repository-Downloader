bf6010a0c93923cc87815760d2d484493982e9da
Christian Deme <christian.deme@cleanercode.de>
2022-11-07 16:58:04 +0100

    Set placeholder URI for Swagger UI form to environment variable

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,4 @@
 # Overwrites the defaults for the Swagger UI
 GrapeSwaggerRails.options.url = "api/swagger_doc.json"
 # noinspection RubyResolve
-GrapeSwaggerRails.options.app_url = "http://localhost:3000/"
+GrapeSwaggerRails.options.app_url = ENV.fetch("SWAGGER_UI") { "http://localhost:3000/" }
