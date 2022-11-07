3c498927aee77272f0e8ab3ee1df9e90b0952aef
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-07 12:56:48 +0100

    Adjust spec to be independent of subclasses

--- spec/mappers/shale_custom/mapper_spec.rb
+++ spec/mappers/shale_custom/mapper_spec.rb
@@ -1,48 +1,24 @@
 describe ShaleCustom::Mapper do
+  let(:sub_object) do
+    Class.new(described_class) do
+      def self.name = "TestMapper"
+
+      attribute :string, Shale::Type::String
+      attribute :integer, Shale::Type::Integer
+    end.new(string: "string_value", integer: 0)
+  end
+
   it { expect(described_class.new).to be_a described_class }
   it { expect(described_class.xml_mapping).to be_a ShaleCustom::Xml }
   it { expect(described_class.csv_mapping).to be_a Shale::Mapping::Dict }
   it { expect(described_class.csv_mapping.instance_variable_get(:@render_nil_default)).to be true }
   it { expect(described_class.json_mapping).to be_a Shale::Mapping::Dict }
   it { expect(described_class.json_mapping.instance_variable_get(:@render_nil_default)).to be true }
-  it { expect(AddressMapper.new.class.instance_variable_get(:@xml_mapping).instance_variable_get(:@root)).to eq "address" }
+  it { expect(sub_object.class.instance_variable_get(:@xml_mapping).instance_variable_get(:@root)).to eq "test" }
 
   describe ".to_csv" do
-    let(:address_mapper) { build :address_mapper }
-
-    it { expect(address_mapper).to be_a described_class }
-    it { expect(address_mapper.class.instance_variable_get(:@csv_mapping)).to be_a Shale::Mapping::Dict }
-
-    context "with default parameters headers are enabled" do
-      let(:expected_csv) do
-        <<~CSV
-          city,street,zip
-          ,,
-        CSV
-      end
-
-      it { expect(address_mapper.to_csv).to eq_without_whitespace expected_csv }
-    end
-
-    context "when parameters are overridden" do
-      let(:expected_csv) do
-        <<~CSV
-          ,,
-        CSV
-      end
-
-      it { expect(address_mapper.to_csv(headers: false)).to eq_without_whitespace expected_csv }
-    end
-
-    context "when additional parameters are given" do
-      let(:expected_csv) do
-        <<~CSV
-          city|street|zip
-          ||
-        CSV
-      end
-
-      it { expect(address_mapper.to_csv(col_sep: "|")).to eq_without_whitespace expected_csv }
-    end
+    it { expect(sub_object.to_csv).to eq "string,integer\n#{sub_object.string},#{sub_object.integer}\n" }
+    it { expect(sub_object.to_csv(headers: false)).to eq "#{sub_object.string},#{sub_object.integer}\n" }
+    it { expect(sub_object.to_csv(col_sep: "|")).to eq "string|integer\n#{sub_object.string}|#{sub_object.integer}\n" }
   end
 end
