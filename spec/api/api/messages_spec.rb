cfa4458ae0f5fb4819246760a4c36ebbe74d239c
Christian Deme <christian.deme@cleanercode.de>
2022-11-07 17:00:06 +0100

    Refactor specs in messages_spec.rb

--- spec/api/api/messages_spec.rb
+++ spec/api/api/messages_spec.rb
@@ -1,35 +1,15 @@
 describe "API::Messages" do
-  describe "GET /api" do
-    it do
-      get "/api/not_existing_path"
-      expect(response).to have_http_status(:not_found)
-    end
+  let(:api_path) { "/api/messages" }
+
+  it do
+    get "/api/not_existing_path"
+    expect(response).to have_http_status(:not_found)
   end
 
   describe "GET /api/messages" do
-    let(:api_path) { "/api/messages" }
-    let(:not_valid_path) { "not_valid_path" }
-    let(:valid_id) { 0 }
-    let(:invalid_id) { 10000000 }
-
     it "gets all messages successfully" do
       get api_path
       expect(response).to have_http_status(:ok)
-    end
-
-    it "gets one message successful" do
-      get "#{api_path}/#{valid_id}"
-      expect(response).to have_http_status(:ok)
-    end
-
-    it "to get one message unsuccessful" do
-      get "#{api_path}/#{invalid_id}"
-      expect(response).to have_http_status(:not_found)
-    end
-
-    it "sends a bad request" do
-      get "#{api_path}/#{not_valid_path}"
-      expect(response).to have_http_status(:bad_request)
     end
 
     it "gets json header" do
@@ -45,6 +25,30 @@ describe "API::Messages" do
     it "gets json header even http accept is not valid" do
       get api_path, headers: {"HTTP_ACCEPT" => "application/not_valid_header"}
       expect(response.header["Content-Type"]).to eq "application/json"
+    end
+  end
+
+  describe "GET /api/messages/:id" do
+    let(:valid_id) { 0 }
+    let(:invalid_id) { 10000000 }
+
+    it "gets one message successful" do
+      get "#{api_path}/#{valid_id}"
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "to get one message unsuccessful" do
+      get "#{api_path}/#{invalid_id}"
+      expect(response).to have_http_status(:not_found)
+    end
+
+    describe "GET /api/messages/:path" do
+      let(:not_valid_path) { "not_valid_path" }
+
+      it "sends a bad request" do
+        get "#{api_path}/#{not_valid_path}"
+        expect(response).to have_http_status(:bad_request)
+      end
     end
   end
 end
