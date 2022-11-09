891e8b5776d9430f40b01c6c5a5a808a4861ac91
Christian Deme <christian.deme@cleanercode.de>
2022-11-09 17:10:20 +0100

    Add environment variable for host URI and update hint in swagger_doc_spec.rb

--- spec/grape/api/swagger_doc_spec.rb
+++ spec/grape/api/swagger_doc_spec.rb
@@ -3,7 +3,7 @@ describe "SWAGGER" do
     {info: {title: "API title", version: "0.0.1"},
      swagger: "2.0",
      produces: %w[application/json application/xml],
-     host: "www.example.com", # HINT: default value for host URI
+     host: ENV["HOST_URI"], # HINT: Default value for host URI
      basePath: "/api",
      tags: [{name: "messages", description: "Operations about messages"}],
      paths: {"/v1/messages": {get: {description: "Return list of messages",
