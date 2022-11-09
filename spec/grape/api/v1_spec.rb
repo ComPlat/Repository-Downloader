4651c9df965769b31eaca1b135b495289e206848
Christian Deme <christian.deme@cleanercode.de>
2022-11-09 15:08:12 +0100

    Remove redundant spec

--- spec/grape/api/v1_spec.rb
+++ spec/grape/api/v1_spec.rb
@@ -1,14 +1,5 @@
 describe "API::V1" do
   describe "GET /api/v1/:path" do
-    context "when path does exist" do
-      let(:args) { {city: "Town", street: "Elm Street 1", zip: "12345"} }
-
-      before { get "/api/v1/messages" }
-
-      it { expect(response).to have_http_status(:ok) }
-      it { expect(response.body).to eq MappersPresenter.new(AddressMapper, [args, args]).to_json }
-    end
-
     context "when path does not exist" do
       before { get "/api/v1/not_existing_path" }
 
