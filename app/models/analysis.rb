8e44033b860bcf6cf79a0b0e7ac87e6304cc4dc9
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-10 23:13:33 +0100

    STI hacking to keep Analysis as model name, even if element_type for it is actually "Container"

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,2 +1,4 @@
 class Analysis < Publication
+  # HINT: Analysis == `select * from toap_publications where element_type = "Container"`
+  def self.sti_name = "Container"
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -2,5 +2,8 @@ class Publication < ApplicationRecord
   self.table_name = "toap_publications"
   self.inheritance_column = "element_type"
 
+  # HINT: Analysis == `select * from toap_publications where element_type = "Container"`
+  def self.find_sti_class(type_name) = type_name == "Container" ? Analysis : super
+
   attr_readonly(*attribute_names)
 end
