7e40edee16b103a9296ffdb1c0db779723b6d070
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-10 23:25:35 +0100

    adding abstract specs for STI hacks

--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -1,3 +1,8 @@
 describe Publication do
   it_behaves_like "Publication"
+
+  describe ".find_sti_class" do
+    it { expect(described_class.find_sti_class("Container")).to eq Analysis }
+    it { expect(described_class.find_sti_class("Reaction")).to eq Reaction }
+  end
 end
