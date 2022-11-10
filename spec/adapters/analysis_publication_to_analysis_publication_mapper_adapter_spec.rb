382054723a39f866583314bcbc195bee2137a956
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-10 22:15:03 +0100

    fixing spec of AnalysisPublicationToAnalysisPublicationMapperAdapter

--- spec/adapters/analysis_publication_to_analysis_publication_mapper_adapter_spec.rb
+++ spec/adapters/analysis_publication_to_analysis_publication_mapper_adapter_spec.rb
@@ -1,15 +1,15 @@
 describe AnalysisPublicationToAnalysisPublicationMapperAdapter do
+  let(:analysis_publication) { build :analysis_publication }
+
   describe ".new" do
     subject { described_class.new(analysis_publication) }
 
-    xit { is_expected.to be_a described_class }
+    it { is_expected.to be_a described_class }
   end
 
   describe "#mapper_args" do
     subject { described_class.new(analysis_publication).mapper_args }
 
-    let(:analysis_publication) { build :analysis_publication }
-
-    xit { is_expected.to eq "" }
+    it { is_expected.to be_nil }
   end
 end
