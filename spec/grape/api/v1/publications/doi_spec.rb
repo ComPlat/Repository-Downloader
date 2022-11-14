ad84611bb39a0ce86ea5261b18eb6d9a84300dc4
Christian Deme <christian.deme@cleanercode.de>
2022-11-14 16:09:54 +0100

    Update specs for updated namespace publications

--- spec/grape/api/v1/publications/doi_spec.rb
+++ spec/grape/api/v1/publications/doi_spec.rb
@@ -2,8 +2,8 @@ describe "API::V1::Publication::Doi" do
   let(:args) { {city: "Town", street: "Doi Street 1", zip: "12345"} }
   let(:address_mapper) { AddressMapper.new(**args) }
 
-  describe "GET /api/v1/publication/doi" do
-    before { get "/api/v1/publication/doi/" }
+  describe "GET /api/v1/publications/doi" do
+    before { get "/api/v1/publications/doi/" }
 
     it { expect(response).to have_http_status(:ok) }
     it { expect(response.body).to eq MappersPresenter.new(AddressMapper, [args, args]).to_json }
