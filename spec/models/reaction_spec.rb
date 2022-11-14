5bb62c7de16fb7a6196edc1e723dbe4aeb459998
Christian Deme <christian.deme@cleanercode.de>
2022-11-14 15:46:28 +0100

    Add specs for Reaction model

--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -1,3 +1,8 @@
 describe Reaction do
   it_behaves_like "Publication"
+
+  it { expect(described_class.sti_name).to eq "Reaction" }
+  it { expect { create :reaction }.to change(described_class, :count).from(0).to(1) }
+  it { expect(create(:reaction)).to be_valid }
+  it { expect(create(:reaction)).to be_persisted }
 end
