745d63b6070d86fc4afd1e107daa44030f6aeaed
Christian Deme <christian.deme@cleanercode.de>
2022-11-14 16:08:46 +0100

    Update specs for updated namespace publications

--- spec/grape/api/v1/publications/chemotion_id_spec.rb
+++ spec/grape/api/v1/publications/chemotion_id_spec.rb
@@ -2,8 +2,8 @@ describe "API::V1::Publication::ChemotionId" do
   let(:args) { {city: "Town", street: "Chemotion Street 1", zip: "12345"} }
   let(:address_mapper) { AddressMapper.new(**args) }
 
-  describe "GET /api/v1/publication/chemotion_id" do
-    before { get "/api/v1/publication/chemotion_id/" }
+  describe "GET /api/v1/publications/chemotion_id" do
+    before { get "/api/v1/publications/chemotion_id/" }
 
     it { expect(response).to have_http_status(:ok) }
     it { expect(response.body).to eq MappersPresenter.new(AddressMapper, [args, args]).to_json }
