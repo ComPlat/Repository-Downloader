ee2baba782953586b3f89719d24142e84d316d9c
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-14 18:52:24 +0100

    Refactor of PublicationRepository for now existing primary key

--- app/repositories/publication_repository.rb
+++ app/repositories/publication_repository.rb
@@ -1,7 +1,5 @@
 class PublicationRepository
   def self.find_by_doi!(doi) = Publication.find_by!("taggable_data @> ?", {doi:}.to_json)
 
-  # HINT: id is NOT the primary key of toap_publications, because it has none!
-  # rubocop:disable Rails/FindById
-  def self.find_by_chemotion_id!(chemotion_id) = Publication.find_by!(id: chemotion_id)
+  def self.find_by_chemotion_id!(chemotion_id) = Publication.find chemotion_id
 end
