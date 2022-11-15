62b0b54bf1e1c15c03cd96a7de9c193878c5000c
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-15 11:57:19 +0100

    Add fields for AnalysisToAnalysisMapperAdapter

--- app/adapters/analysis_to_analysis_mapper_adapter.rb
+++ app/adapters/analysis_to_analysis_mapper_adapter.rb
@@ -7,21 +7,25 @@ class AnalysisToAnalysisMapperAdapter
     (public_methods(false) - [:to_h]).index_with { |method| public_send method }
   end
 
-  def context = "https://schema.org/"
+  def context = "https://schema.org/" # HINT: becomes @context in mapper
 
-  def type = "AnalysisEntity"
+  def type = "AnalysisEntity" # HINT: becomes @type in mapper
 
-  def id = ""
+  def id = url
 
-  def ontologies = ""
+  def ontologies = @analysis.extended_metadata["kind"].split("|").last.strip
 
-  def title = ""
+  def title = Nokogiri::XML(@analysis.metadata_xml).search("title").map { |title| title.text }.join(";")
 
-  def descriptions = ""
+  def descriptions = "" # HINT:
 
-  def url = ""
+  def url = "https://dx.doi.org/#{@analysis.taggable_data["analysis_doi"]}"
 
-  def identifier = ""
+  def identifier = @analysis.chemotion_id
 
-  def datasetList = {}
+  def dataset_list = {numberOfItems:, itemListElement: DataSetListAdapter.new(@analysis).item_list_element}
+
+  private
+
+  def numberOfItems = @analysis.attachments.count
 end
