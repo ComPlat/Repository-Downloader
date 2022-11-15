e26869ef38585f16706b995bc842c177c2ea6c80
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-15 12:50:34 +0100

    Remove example mappers with their factories and specs

--- app/mappers/address_mapper.rb
+++
@@ -1,5 +0,0 @@
-class AddressMapper < ShaleCustom::Mapper
-  attribute :city, Shale::Type::String
-  attribute :street, Shale::Type::String
-  attribute :zip, Shale::Type::String
-end
--- app/mappers/person_mapper.rb
+++
@@ -1,8 +0,0 @@
-class PersonMapper < ShaleCustom::Mapper
-  attribute :first_name, Shale::Type::String
-  attribute :last_name, Shale::Type::String
-  attribute :age, Shale::Type::Integer
-  attribute :married, Shale::Type::Boolean, default: -> { false }
-  attribute :hobbies, Shale::Type::String, collection: true
-  attribute :address, AddressMapper
-end
