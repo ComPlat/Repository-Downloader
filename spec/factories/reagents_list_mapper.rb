e8440ed0b09c2f0c392cacf5f33c6486968fb4dd
Christian Deme <christian.deme@cleanercode.de>
2022-11-15 19:24:46 +0100

    Update factory of ReagentsListMapper

--- spec/factories/reagents_list_mapper.rb
+++ spec/factories/reagents_list_mapper.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :reagents_list_mapper do
     trait :with_all_args do
-      numberOfItems { 5 }
+      numberOfItems { 1 }
       itemListElement { build :reagents_list_item_list_element_mapper, :with_all_args }
     end
   end
