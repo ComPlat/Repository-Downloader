5bb3b7a03f84905bb5b713cd42f8e9a6ca7a291a
Christian Deme <christian.deme@cleanercode.de>
2022-11-15 12:05:19 +0100

    Create specs for Reaction model

---
+++ spec/models/reaction_spec.rb
@@ -0,0 +1,16 @@
+describe Reaction do
+  it_behaves_like "Publication"
+
+  it { expect(described_class.sti_name).to eq "Reaction" }
+  it { expect { create :reaction }.to change(described_class, :count).from(0).to(1) }
+  it { expect(create(:reaction)).to be_valid }
+  it { expect(create(:reaction)).to be_persisted }
+
+  describe "#chemotion_id" do
+    subject(:chemotion_id) { reaction.chemotion_id }
+
+    let(:reaction) { create :reaction, :with_realistic_attributes }
+
+    it { is_expected.to eq "CRR-#{reaction.id}" }
+  end
+end
