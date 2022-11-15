cbd9e55a8a24f9f7857a6e6f2687cad5981f024f
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-15 14:56:17 +0100

    adding hint comment to Analysis and also adding missing spec

--- spec/models/analysis_spec.rb
+++ spec/models/analysis_spec.rb
@@ -26,4 +26,12 @@ describe Analysis do
 
     it { is_expected.to eq "CRD-#{analysis.id}" }
   end
+
+  describe "#present_to_api" do
+    subject(:present_to_api) { analysis.present_to_api }
+
+    let(:analysis) { create :analysis, :with_realistic_attributes }
+
+    it { is_expected.to eq "" }
+  end
 end
