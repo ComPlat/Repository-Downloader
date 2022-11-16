e9c9d3eb41e37033e7b83a4f00a88c6a2ef6062f
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-16 18:27:16 +0100

    Added Reaction#present_to_api

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,7 +2,13 @@ class Reaction < Publication
   # HINT: Reaction == `select * from toap_publications where element_type = "Reaction"`
   def self.sti_name = "Reaction"
 
-  def present_to_api = {}
-
   def chemotion_id = "CRR-#{id}"
+
+  def present_to_api = ReactionMapper.from_hash to_reaction_mapper_hash
+
+  private
+
+  def to_reaction_mapper_hash = to_reaction_mapper.to_h
+
+  def to_reaction_mapper = ReactionToReactionMapperAdapter.new self
 end
--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -6,19 +6,27 @@ describe Reaction do
   it { expect(create(:reaction)).to be_valid }
   it { expect(create(:reaction)).to be_persisted }
 
-  describe "#present_to_api" do
-    subject(:present_to_api) { reaction.present_to_api }
-
-    let(:reaction) { create :reaction, :with_realistic_attributes }
-
-    it { is_expected.to eq({}) }
-  end
-
   describe "#chemotion_id" do
     subject(:chemotion_id) { reaction.chemotion_id }
 
     let(:reaction) { create :reaction, :with_realistic_attributes }
 
     it { is_expected.to eq "CRR-#{reaction.id}" }
+  end
+
+  describe "#present_to_api" do
+    subject(:present_to_api) { reaction.present_to_api }
+
+    let(:reaction) { create :reaction, :with_realistic_attributes }
+    let(:reaction_to_reaction_mapper_adapter) { ReactionToReactionMapperAdapter.new reaction }
+    let(:reaction_to_reaction_mapper_adapter_hash) { reaction_to_reaction_mapper_adapter.to_h }
+    let(:reaction_mapper) { ReactionMapper.from_hash reaction_to_reaction_mapper_adapter_hash }
+
+    it { expect(present_to_api).to be_a ReactionMapper }
+    it { expect(present_to_api.identifier).to eq reaction.chemotion_id }
+    it { expect(present_to_api.context).to eq reaction_to_reaction_mapper_adapter.context }
+    it { expect(present_to_api.to_json).to eq reaction_mapper.to_json }
+    it { expect(present_to_api.to_xml).to eq reaction_mapper.to_xml }
+    it { expect(present_to_api.to_csv).to eq reaction_mapper.to_csv }
   end
 end
