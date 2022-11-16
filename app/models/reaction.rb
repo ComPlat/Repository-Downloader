dc254b643e2e7233aff3168df987f7c5c52aa5c1
Christian Deme <christian.deme@cleanercode.de>
2022-11-16 17:23:00 +0100

    Add methods present_to_api and chemotion_id to Reaction, add specs for it

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,8 @@
 class Reaction < Publication
   # HINT: Reaction == `select * from toap_publications where element_type = "Reaction"`
   def self.sti_name = "Reaction"
+
+  def present_to_api = {}
+
+  def chemotion_id = "CRR-#{id}"
 end
--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -5,4 +5,20 @@ describe Reaction do
   it { expect { create :reaction }.to change(described_class, :count).from(0).to(1) }
   it { expect(create(:reaction)).to be_valid }
   it { expect(create(:reaction)).to be_persisted }
+
+  describe "#present_to_api" do
+    subject(:present_to_api) { reaction.present_to_api }
+
+    let(:reaction) { create :reaction, :with_realistic_attributes }
+
+    it { is_expected.to eq({}) }
+  end
+
+  describe "#chemotion_id" do
+    subject(:chemotion_id) { reaction.chemotion_id }
+
+    let(:reaction) { create :reaction, :with_realistic_attributes }
+
+    it { is_expected.to eq "CRR-#{reaction.id}" }
+  end
 end
