10eb9977cece9f4d196b8a3db878c25f83f65836
Christian Deme <christian.deme@cleanercode.de>
2022-11-16 12:48:57 +0100

    Connect ChemotionID endpoint with PublicationPresenter

--- app/grape/api/v1/publications/chemotion_id.rb
+++ app/grape/api/v1/publications/chemotion_id.rb
@@ -5,23 +5,13 @@ module API
         version "v1", using: :path
 
         namespace "/publications/chemotion_id" do
-          desc "Return list of publications"
-          get do
-            args = {city: "Town", street: "Chemotion Street 1", zip: "12345"}
-            present MappersPresenter.new AddressMapper, [args, args]
-          end
-
           desc "Get one publication via ChemotionID"
           params do
             requires :id, type: Integer, desc: "ChemotionID"
           end
           route_param :id, type: Integer do
             get do
-              if params[:id] == 1
-                AddressMapper.new city: "Town", street: "Chemotion Street 1", zip: "12345"
-              else
-                error!("Invalid message: #{params[:id]}", 404) unless present
-              end
+              return error!("Invalid ChemotionID: #{params[:id]}", 404) unless present PublicationPresenter.present_by_chemotion_id(params[:id])
             end
           end
         end
