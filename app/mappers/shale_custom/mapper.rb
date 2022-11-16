dcddbe3e0a12000534dcebe0dbc69a226aa0fa89
Christian Deme <christian.deme@cleanercode.de>
2022-11-16 12:42:20 +0100

    Implementing ShaleCustom::Mapper.from_hash to avoid repetition (or errors by forgetting it) .deep_stringify_keys

--- app/mappers/shale_custom/mapper.rb
+++ app/mappers/shale_custom/mapper.rb
@@ -4,6 +4,12 @@ module ShaleCustom
     @csv_mapping = Shale::Mapping::Dict.new(render_nil_default: true)
     @xml_mapping = ShaleCustom::Xml.new
 
+    def self.from_hash(hash)
+      raise ArgumentError, "hash needs to be a Hash" unless hash.is_a? Hash
+
+      super hash.deep_stringify_keys
+    end
+
     def initialize(**args)
       super(**args)
       self.class.instance_variable_get(:@xml_mapping).root self.class.name.gsub("Mapper", "").downcase
--- spec/mappers/shale_custom/mapper_spec.rb
+++ spec/mappers/shale_custom/mapper_spec.rb
@@ -1,12 +1,13 @@
 describe ShaleCustom::Mapper do
-  let(:sub_object) do
+  let(:sub_class) do
     Class.new(described_class) do
       def self.name = "TestMapper"
 
       attribute :string, Shale::Type::String
       attribute :integer, Shale::Type::Integer
-    end.new(string: "string_value", integer: 0)
+    end
   end
+  let(:sub_object) { sub_class.new(string: "string_value", integer: 0) }
 
   it { expect(described_class.new).to be_a described_class }
   it { expect(described_class.xml_mapping).to be_a ShaleCustom::Xml }
@@ -16,7 +17,15 @@ describe ShaleCustom::Mapper do
   it { expect(described_class.json_mapping.instance_variable_get(:@render_nil_default)).to be true }
   it { expect(sub_object.class.instance_variable_get(:@xml_mapping).instance_variable_get(:@root)).to eq "test" }
 
-  describe ".to_csv" do
+  describe ".from_hash" do
+    it { expect { sub_class.from_hash(nil) }.to raise_error ArgumentError, "hash needs to be a Hash" }
+    it { expect(sub_class.from_hash({})).to be_a sub_class }
+    it { expect(sub_class.from_hash({})).to have_attributes(string: nil, integer: nil) }
+    it { expect(sub_class.from_hash({string: "", integer: 0})).to have_attributes(string: "", integer: 0) }
+    it { expect(sub_class.from_hash({"string" => "", "integer" => 0})).to have_attributes(string: "", integer: 0) }
+  end
+
+  describe "#to_csv" do
     it { expect(sub_object.to_csv).to eq "string,integer\n#{sub_object.string},#{sub_object.integer}\n" }
     it { expect(sub_object.to_csv(headers: false)).to eq "#{sub_object.string},#{sub_object.integer}\n" }
     it { expect(sub_object.to_csv(col_sep: "|")).to eq "string|integer\n#{sub_object.string}|#{sub_object.integer}\n" }
