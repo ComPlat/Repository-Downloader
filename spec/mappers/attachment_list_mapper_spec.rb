b6ea775cdc5fc8cf66a77ad6c430710175127b94
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-16 16:02:38 +0100

    Add expected_json to attachment_list_mapper_spec.rb

--- spec/mappers/attachment_list_mapper_spec.rb
+++ spec/mappers/attachment_list_mapper_spec.rb
@@ -5,32 +5,41 @@ describe AttachmentListMapper do
     context "when called without any arguments" do
       let(:attachment_list_mapper) { build :attachment_list_mapper }
 
+      let(:expected_json) do
+        <<~JSON
+          {
+            "numberOfItems": #{expected_json_nil_render_value},
+            "itemListElement":[]
+          }
+        JSON
+      end
+
       it { expect(attachment_list_mapper).to be_a described_class }
       it { expect(attachment_list_mapper).to be_a ShaleCustom::Mapper }
       it { expect(attachment_list_mapper.numberOfItems).to be_nil }
       it { expect(attachment_list_mapper.itemListElement).to match_array([]) }
 
-      describe "#to_json" do
-        let(:expected_json) do
-          <<~JSON
-            {
-              "numberOfItems": #{expected_json_nil_render_value},
-              "itemListElement":[]
-            }
-          JSON
-        end
-
-        it { expect(attachment_list_mapper.to_json).to eq_without_whitespace expected_json }
-      end
+      it { expect(attachment_list_mapper.to_json).to eq_without_whitespace expected_json }
     end
 
     context "when called with all arguments" do
       let(:args) { attributes_for :attachment_list_mapper, :with_all_args_nested_structures_as_mappers }
       let(:attachment_list_mapper) { described_class.new(**args) }
 
+      let(:expected_json) do
+        <<~JSON
+          {
+            "numberOfItems": #{args[:numberOfItems]},
+            "itemListElement": [#{args[:itemListElement].first.to_json}, #{args[:itemListElement].second.to_json}]
+          }
+        JSON
+      end
+
       it { expect(attachment_list_mapper).to be_a described_class }
       it { expect(attachment_list_mapper.numberOfItems).to eq args[:numberOfItems] }
       it { expect(attachment_list_mapper.itemListElement).to eq args[:itemListElement] }
+
+      it { expect(JSON.parse(attachment_list_mapper.to_json)).to eq JSON.parse expected_json }
     end
   end
 
