025315802ed26950a2c553349a7e1e68ae856e54
Christian Deme <christian.deme@cleanercode.de>
2022-11-16 12:49:30 +0100

    Update spec for API::V1::Publication::ChemotionId

--- spec/grape/api/v1/publications/chemotion_id_spec.rb
+++ spec/grape/api/v1/publications/chemotion_id_spec.rb
@@ -1,12 +1,13 @@
 describe "API::V1::Publication::ChemotionId" do
-  let(:args) { {city: "Town", street: "Chemotion Street 1", zip: "12345"} }
-  let(:address_mapper) { AddressMapper.new(**args) }
+  let(:analysis) { create :analysis, :with_realistic_attributes }
 
-  describe "GET /api/v1/publications/chemotion_id" do
-    before { get "/api/v1/publications/chemotion_id/" }
+  describe "GET /api/v1/publications/chemotion_id/:chemotion_id" do
+    before { get "/api/v1/publications/chemotion_id/#{analysis.id}" }
+
+    let(:expected_json) { PublicationPresenter.present_by_chemotion_id(analysis.id).to_json }
 
     it { expect(response).to have_http_status(:ok) }
-    it { expect(response.body).to eq MappersPresenter.new(AddressMapper, [args, args]).to_json }
+    it { expect(response.body).to eq expected_json }
     it { expect(response.content_type).to eq("application/json") }
   end
 end
