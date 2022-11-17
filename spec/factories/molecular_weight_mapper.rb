59b2dd9366574804dd6509b6a68bb0bbb7e21ed9
Christian Deme <christian.deme@cleanercode.de>
2022-11-17 17:43:56 +0100

    Update MolecularWeightMapper, add factory and specs for it

---
+++ spec/factories/molecular_weight_mapper.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :molecular_weight_mapper do
+    trait :with_all_args do
+      value { 218.20870000000002 }
+    end
+  end
+end
---
+++ spec/mappers/molecular_weight_mapper_spec.rb
@@ -0,0 +1,70 @@
+describe MolecularWeightMapper do
+  let(:expected_json_nil_render_value) { "null" }
+
+  describe ".new" do
+    context "when called without any arguments" do
+      let(:molecular_weight_mapper) { build :molecular_weight_mapper }
+
+      it { expect(molecular_weight_mapper).to be_a described_class }
+      it { expect(molecular_weight_mapper).to be_a ShaleCustom::Mapper }
+      it { expect(molecular_weight_mapper.value).to be_nil }
+    end
+
+    context "when called with all arguments" do
+      let(:args) { attributes_for :molecular_weight_mapper, :with_all_args }
+      let(:molecular_weight_mapper) { described_class.new(**args) }
+
+      it { expect(molecular_weight_mapper).to be_a described_class }
+      it { expect(molecular_weight_mapper).to be_a ShaleCustom::Mapper }
+      it { expect(molecular_weight_mapper.value).to eq args[:value] }
+    end
+
+    describe ".from_hash" do
+      context "when called without any arguments" do
+        let(:molecular_weight_mapper) { build :molecular_weight_mapper }
+
+        it { expect(molecular_weight_mapper).to be_a described_class }
+        it { expect(molecular_weight_mapper.value).to be_nil }
+      end
+
+      context "when called with all arguments" do
+        let(:args) { attributes_for(:molecular_weight_mapper, :with_all_args) }
+        let(:molecular_weight_mapper) { described_class.from_hash args }
+
+        it { expect(molecular_weight_mapper).to be_a described_class }
+        it { expect(molecular_weight_mapper.value).to eq args[:value] }
+      end
+    end
+
+    describe "#to_json" do
+      context "when called without any arguments" do
+        let(:molecular_weight_mapper) { build :molecular_weight_mapper }
+
+        let(:expected_json) do
+          <<~JSON
+            {
+              "value": #{expected_json_nil_render_value}
+            }
+          JSON
+        end
+
+        it { expect(molecular_weight_mapper.to_json).to eq_without_whitespace expected_json }
+      end
+
+      context "when called with all arguments" do
+        let(:args) { attributes_for(:molecular_weight_mapper, :with_all_args) }
+        let(:molecular_weight_mapper) { described_class.from_hash args }
+
+        let(:expected_json) do
+          <<~JSON
+            {
+              "value": #{args[:value]}
+            }
+          JSON
+        end
+
+        it { expect(molecular_weight_mapper.to_json).to eq_without_whitespace expected_json }
+      end
+    end
+  end
+end
