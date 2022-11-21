8f3bec5abb540cca67c8be352e25c129ebb06243
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-21 14:14:01 +0100

    Improve readability, remove unnecessary method call

--- app/adapters/analysis_adapter/data_set_list/item_list_element/attachment_list/item_list_element_adapter.rb
+++ app/adapters/analysis_adapter/data_set_list/item_list_element/attachment_list/item_list_element_adapter.rb
@@ -2,7 +2,11 @@ module AnalysisAdapter::DataSetList::ItemListElement::AttachmentList
   class ItemListElementAdapter
     include Hashable
 
-    def initialize(analysis, ds_id, attachments) = (@analysis = analysis, @ds_id = ds_id, @attachments = attachments)
+    def initialize(analysis, ds_id, attachments)
+      @analysis = analysis
+      @ds_id = ds_id
+      @attachments = attachments
+    end
 
     def type = @type ||= "AttachmentEntity"
 
@@ -10,6 +14,6 @@ module AnalysisAdapter::DataSetList::ItemListElement::AttachmentList
 
     def filename = @filename ||= @attachments.first.filename
 
-    def filepath = @filepath ||= "data/#{@analysis.first.chemotion_id}" # TODO: Check if correct.
+    def filepath = @filepath ||= "data/#{@analysis.chemotion_id}"
   end
 end
