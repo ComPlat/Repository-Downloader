61ad2eee38af4c2d399ef8d7bc13258bf18f43d7
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-21 10:50:16 +0100

    Implement analysis adapters, adjust specs for namespaces

---
+++ spec/adapters/analysis_adapter/data_set_list/item_list_element_adapter_spec.rb
@@ -0,0 +1,59 @@
+describe AnalysisAdapter::DataSetList::ItemListElementAdapter do
+  let(:analysis) { create :analysis, :with_realistic_attributes, element_id: 1 }
+  let(:data_set_attachment) {
+    create :attachment,
+      ds_id: 2,
+      ana_id: analysis.element_id,
+      identifier: "a63e278b-22f2-4da3-955f-e80e197bc853",
+      filename: "BJ68_1H.zip"
+  }
+  let(:data_set_list_adapter) { described_class.new analysis, data_set_attachment.ds_id, [data_set_attachment] }
+
+  describe ".new" do
+    subject { data_set_list_adapter }
+
+    it { is_expected.to be_a described_class }
+  end
+
+  describe "#type" do
+    subject { data_set_list_adapter.type }
+
+    it { is_expected.to eq "DatasetEntity" }
+  end
+
+  describe "#identifier" do
+    subject { data_set_list_adapter.identifier }
+
+    it { is_expected.to eq data_set_attachment.ds_id }
+  end
+
+  describe "#name" do
+    subject { data_set_list_adapter.name }
+
+    it { is_expected.to eq "BJ68_1H" } # TODO: Implement this correctly!
+  end
+
+  describe "#Instrument" do
+    subject { data_set_list_adapter.Instrument }
+
+    it { is_expected.to eq data_set_attachment.instrument }
+    it { is_expected.to be_a String }
+  end
+
+  describe "#descriptions" do
+    subject { data_set_list_adapter.descriptions }
+
+    it { is_expected.to eq "" } # TODO: Implement this correctly!
+  end
+
+  describe "#attachmentList" do
+    subject { data_set_list_adapter.attachmentList }
+
+    let(:expected_hash) {
+      AnalysisAdapter::DataSetList::ItemListElement::AttachmentListAdapter.new(analysis, [data_set_attachment]).to_h
+    }
+
+    it { is_expected.to eq expected_hash }
+    it { is_expected.to be_a Hash }
+  end
+end
