e2891f1290b83ce6c1a9c8fd0ee0d391066463c1
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-22 13:32:04 +0100

    Removed not needed braces

--- app/adapters/concerns/Hashable.rb
+++ app/adapters/concerns/Hashable.rb
@@ -1,5 +1,5 @@
 module Hashable
   extend ActiveSupport::Concern
 
-  def to_h = (@to_h ||= (public_methods(false) - [:to_h]).index_with { |method| public_send method })
+  def to_h = @to_h ||= (public_methods(false) - [:to_h]).index_with { |method| public_send method }
 end
