eaa4df33921f32c8a856b4d7f0c2a9be6bfe87c7
Christian Deme <christian.deme@cleanercode.de>
2022-11-22 18:31:46 +0100

    Fix Rubocop issues

--- app/adapters/sample_adapter/dct_element_adapter.rb
+++ app/adapters/sample_adapter/dct_element_adapter.rb
@@ -2,8 +2,8 @@ module SampleAdapter
   class DctElementAdapter
     # TODO: Create specs for me!
     include Hashable
-    def self.id = @id ||= "https://bioschemas.org/profiles/MolecularEntity/0.5-RELEASE"
+    def self.id = "https://bioschemas.org/profiles/MolecularEntity/0.5-RELEASE"
 
-    def self.type = @type ||= "CreativeWork"
+    def self.type = "CreativeWork"
   end
 end
