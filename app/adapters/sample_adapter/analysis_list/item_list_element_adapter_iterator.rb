e354c708aba2bc3ca0931eee2caf21d3ae188630
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-23 18:47:15 +0100

    refactored SampleAdapter::AnalysisList::ItemListElementAdapterIterator

--- app/adapters/sample_adapter/analysis_list/item_list_element_adapter_iterator.rb
+++ app/adapters/sample_adapter/analysis_list/item_list_element_adapter_iterator.rb
@@ -3,9 +3,7 @@ module SampleAdapter
     class ItemListElementAdapterIterator
       def initialize(sample) = @sample = sample
 
-      def to_a = @to_a ||= @sample.analyses.map do |analysis|
-        RootAdapters::AnalysisToAnalysisMapperAdapter.new(analysis).to_h
-      end
+      def to_a = @to_a ||= @sample.analyses.map { |analysis| RootAdapters::AnalysisToAnalysisMapperAdapter.new(analysis).to_h }
     end
   end
 end
