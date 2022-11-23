5713bf3f2190d89de9e48ec3d5de9d061d7092fe
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-23 18:41:27 +0100

    Refactored SampleAdapter::AnalysisList::ItemListElementAdapter

--- app/adapters/sample_adapter/analysis_list/item_list_element_adapter.rb
+++ app/adapters/sample_adapter/analysis_list/item_list_element_adapter.rb
@@ -1,13 +1,11 @@
-module SampleAdapter
-  module AnalysisList
-    class ItemListElementAdapter
-      def initialize(sample) = @sample = sample
+module SampleAdapter::AnalysisList
+  class ItemListElementAdapter
+    def initialize(sample) = @sample = sample
 
-      def itemListElement = @item_list_element ||= item_list_element_array
+    def itemListElement = @item_list_element ||= item_list_element.to_a
 
-      private
+    private
 
-      def item_list_element_array = @item_list_element_array ||= ItemListElementAdapterIterator.new(@sample).to_a
-    end
+    def item_list_element = @item_list_element ||= ItemListElementAdapterIterator.new(@sample)
   end
 end
