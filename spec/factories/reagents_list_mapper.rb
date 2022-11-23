f6c49b022f4a2a29d2668685cea017fc82d63995
Tobias Vetter <tobias.vetter@mailbox.org>
2022-11-23 18:34:00 +0100

    Adding spec for reagents_list_mapper factory and making sure numberOfItems always is same als itemListElement.count, also making the collection an array

--- spec/factories/reagents_list_mapper.rb
+++ spec/factories/reagents_list_mapper.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :reagents_list_mapper, class: "ReactionMappers::ReagentsListMapper" do
     trait :with_all_args do
-      numberOfItems { 1 }
-      itemListElement { build :reagents_list_item_list_element_mapper, :with_all_args }
+      numberOfItems { itemListElement.count }
+      itemListElement { [build(:reagents_list_item_list_element_mapper, :with_all_args)] }
     end
   end
 end
