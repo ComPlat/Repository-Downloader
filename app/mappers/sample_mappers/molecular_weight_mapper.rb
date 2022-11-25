3cfac3c9a7233b9dbff9f54181ca5de1cdef263f
Christian Deme <christian.deme@cleanercode.de>
2022-11-25 17:16:59 +0100

    Remove unnecessary json mapping in SampleMappers::MolecularWeightMapper

--- app/mappers/sample_mappers/molecular_weight_mapper.rb
+++ app/mappers/sample_mappers/molecular_weight_mapper.rb
@@ -1,9 +1,5 @@
 module SampleMappers
   class MolecularWeightMapper < ShaleCustom::Mapper
     attribute :value, Shale::Type::Float
-
-    json do
-      map "value", to: :value
-    end
   end
 end
