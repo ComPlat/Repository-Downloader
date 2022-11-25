9224f34895b6264bf9c55fa1434c4023e8f292f2
Christian Deme <christian.deme@cleanercode.de>
2022-11-25 17:13:40 +0100

    Rename conformTo to dct_conformsTo

--- spec/factories/dct_element_mapper.rb
+++ spec/factories/dct_element_mapper.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :dct_element_mapper, class: "SampleMappers::DctElementMapper" do
     trait :with_all_args_nested_structures_as_mappers do
-      conformsTo { build :dct_list_mapper, :with_all_args }
+      dct_conformsTo { build :dct_list_mapper, :with_all_args }
     end
 
     trait :with_all_args_nested_structures_as_hash do
-      conformsTo { attributes_for :dct_list_mapper, :with_all_args }
+      dct_conformsTo { attributes_for :dct_list_mapper, :with_all_args }
     end
   end
 end
