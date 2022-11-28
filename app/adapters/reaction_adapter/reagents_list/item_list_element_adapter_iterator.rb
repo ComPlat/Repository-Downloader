d1fa1d846788eee763e3c0b941e715381f6e339c
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-28 16:17:11 +0100

    Implement ReactionAdapter::ReagentsList::ItemListElementAdapterIterator

---
+++ app/adapters/reaction_adapter/reagents_list/item_list_element_adapter_iterator.rb
@@ -0,0 +1,7 @@
+module ReactionAdapter::ReagentsList
+  class ItemListElementAdapterIterator
+    def initialize(reaction) = @reaction = reaction
+
+    def to_a = @to_a ||= @reaction.samples.map { |sample| ItemListElementAdapter.new(sample) }
+  end
+end
