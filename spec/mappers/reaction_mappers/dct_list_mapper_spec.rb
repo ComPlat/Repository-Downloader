cc0153ed058f8dfac17ce9cdede5451e06ae6bb5
Christian Deme <christian.deme@cleanercode.de>
2022-11-28 16:14:29 +0100

    Refactor spec structure of ReactionMappers::DctListMapper

--- spec/mappers/reaction_mappers/dct_list_mapper_spec.rb
+++ spec/mappers/reaction_mappers/dct_list_mapper_spec.rb
@@ -1,15 +1,41 @@
 describe ReactionMappers::DctListMapper do
   let(:expected_json_nil_render_value) { "null" }
 
-  context "when called without any arguments" do
-    let(:dct_list_mapper) { build :dct_list_mapper }
+  describe ".new" do
+    context "when called without any arguments" do
+      let(:dct_list_mapper) { build :dct_list_mapper }
 
-    it { expect(dct_list_mapper).to be_a described_class }
-    it { expect(dct_list_mapper).to be_a ShaleCustom::Mapper }
-    it { expect(dct_list_mapper.type).to be_nil }
-    it { expect(dct_list_mapper.id).to be_nil }
+      it { expect(dct_list_mapper).to be_a described_class }
+      it { expect(dct_list_mapper).to be_a ShaleCustom::Mapper }
+      it { expect(dct_list_mapper.type).to be_nil }
+      it { expect(dct_list_mapper.id).to be_nil }
+    end
 
-    describe "#to_json" do
+    context "when called with all arguments" do
+      let(:args) { attributes_for :dct_list_mapper, :with_all_args }
+      let(:dct_list_mapper) { described_class.new(**args) }
+
+      it { expect(dct_list_mapper).to be_a described_class }
+      it { expect(dct_list_mapper).to be_a ShaleCustom::Mapper }
+      it { expect(dct_list_mapper.type).to eq args[:type] }
+      it { expect(dct_list_mapper.id).to eq args[:id] }
+    end
+
+    context "when called with some arguments" do
+      let(:args) { attributes_for :dct_list_mapper, :with_all_args, type: nil }
+      let(:dct_list_mapper) { described_class.new(**args) }
+
+      it { expect(dct_list_mapper).to be_a described_class }
+      it { expect(dct_list_mapper).to be_a ShaleCustom::Mapper }
+      it { expect(dct_list_mapper.type).to be_nil }
+      it { expect(dct_list_mapper.id).to eq args[:id] }
+    end
+  end
+
+  describe "to_json" do
+    context "when called without any arguments" do
+      let(:dct_list_mapper) { build :dct_list_mapper }
+
       let(:expected_json) do
         <<~JSON
           {
@@ -21,18 +47,11 @@ describe ReactionMappers::DctListMapper do
 
       it { expect(dct_list_mapper.to_json).to eq_without_whitespace expected_json }
     end
-  end
 
-  context "when called with all arguments" do
-    let(:args) { attributes_for :dct_list_mapper, :with_all_args }
-    let(:dct_list_mapper) { described_class.new(**args) }
+    context "when called with all arguments" do
+      let(:args) { attributes_for :dct_list_mapper, :with_all_args }
+      let(:dct_list_mapper) { described_class.new(**args) }
 
-    it { expect(dct_list_mapper).to be_a described_class }
-    it { expect(dct_list_mapper).to be_a ShaleCustom::Mapper }
-    it { expect(dct_list_mapper.type).to eq args[:type] }
-    it { expect(dct_list_mapper.id).to eq args[:id] }
-
-    describe "#to_json" do
       let(:expected_json) do
         <<~JSON
           {
@@ -44,18 +63,11 @@ describe ReactionMappers::DctListMapper do
 
       it { expect(dct_list_mapper.to_json).to eq_without_whitespace expected_json }
     end
-  end
 
-  context "when called with some arguments" do
-    let(:args) { attributes_for :dct_list_mapper, :with_all_args, type: nil }
-    let(:dct_list_mapper) { described_class.new(**args) }
+    context "when called with some arguments" do
+      let(:args) { attributes_for :dct_list_mapper, :with_all_args, type: nil }
+      let(:dct_list_mapper) { described_class.new(**args) }
 
-    it { expect(dct_list_mapper).to be_a described_class }
-    it { expect(dct_list_mapper).to be_a ShaleCustom::Mapper }
-    it { expect(dct_list_mapper.type).to be_nil }
-    it { expect(dct_list_mapper.id).to eq args[:id] }
-
-    describe "#to_json" do
       let(:expected_json) do
         <<~JSON
           {
