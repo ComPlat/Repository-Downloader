0577709f859bd628db2ba1fd5fcf873a59b0e6d6
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-28 17:57:18 +0100

    Add missing .to_h for ItemListElementAdapter call

--- app/adapters/reaction_adapter/reagents_list/item_list_element_adapter_iterator.rb
+++ app/adapters/reaction_adapter/reagents_list/item_list_element_adapter_iterator.rb
@@ -2,6 +2,6 @@ module ReactionAdapter::ReagentsList
   class ItemListElementAdapterIterator
     def initialize(reaction) = @reaction = reaction
 
-    def to_a = @to_a ||= @reaction.samples.map { |sample| ItemListElementAdapter.new(sample) }
+    def to_a = @to_a ||= @reaction.samples.map { |sample| ItemListElementAdapter.new(sample).to_h }
   end
 end
