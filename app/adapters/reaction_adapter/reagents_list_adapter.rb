dc11f66801898ba2853ee28ef45337db261f652a
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-28 16:26:08 +0100

    Implement ReactionAdapter::ReagentsListAdapter

--- app/adapters/reaction_adapter/reagents_list_adapter.rb
+++ app/adapters/reaction_adapter/reagents_list_adapter.rb
@@ -1,5 +1,15 @@
 module ReactionAdapter
   class ReagentsListAdapter
+    include Hashable
+
     def initialize(reaction) = @reaction = reaction
+
+    def numberOfItems = @number_of_items ||= @reaction.samples.count
+
+    def itemListElement = @item_list_element ||= item_list_element_adapter_iterator.to_a
+
+    private
+
+    def item_list_element_adapter_iterator = @item_list_element_adapter_iterator ||= ReagentsList::ItemListElementAdapterIterator.new(@reaction)
   end
 end
