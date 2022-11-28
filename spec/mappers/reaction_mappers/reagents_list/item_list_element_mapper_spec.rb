37cfda06cfa7a5c9195b4aac8ee736f95fee01ca
Christian Deme <christian.deme@cleanercode.de>
2022-11-28 15:27:29 +0100

    Refactor spec structure of ReactionMappers::ReagentsList::ItemListElementMapper

--- spec/mappers/reaction_mappers/reagents_list/item_list_element_mapper_spec.rb
+++ spec/mappers/reaction_mappers/reagents_list/item_list_element_mapper_spec.rb
@@ -2,21 +2,61 @@ describe ReactionMappers::ReagentsList::ItemListElementMapper do
   let(:expected_json_nil_render_value) { "null" }
   let(:reagents_list_item_list_element_mapper) { build :reagents_list_item_list_element_mapper, :with_all_args }
 
-  context "when called without any arguments" do
-    let(:reagents_list_item_list_element_mapper) { build :reagents_list_item_list_element_mapper }
+  describe ".new" do
+    context "when called without any arguments" do
+      let(:reagents_list_item_list_element_mapper) { build :reagents_list_item_list_element_mapper }
 
-    it { expect(reagents_list_item_list_element_mapper).to be_a described_class }
-    it { expect(reagents_list_item_list_element_mapper).to be_a ShaleCustom::Mapper }
-    it { expect(reagents_list_item_list_element_mapper.type).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.dct_conformsTo).to eq [] }
-    it { expect(reagents_list_item_list_element_mapper.id).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.identifier).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.name).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.molecularFormula).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.inChIKey).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.smiles).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper).to be_a described_class }
+      it { expect(reagents_list_item_list_element_mapper).to be_a ShaleCustom::Mapper }
+      it { expect(reagents_list_item_list_element_mapper.type).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.dct_conformsTo).to eq [] }
+      it { expect(reagents_list_item_list_element_mapper.id).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.identifier).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.name).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.molecularFormula).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.inChIKey).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.smiles).to be_nil }
+    end
 
-    describe "#to_json" do
+    context "when called with all arguments" do
+      let(:args) { attributes_for :reagents_list_item_list_element_mapper, :with_all_args }
+      let(:reagents_list_item_list_element_mapper) { described_class.new(**args) }
+      let(:dct_list_mapper) { build :dct_list_mapper, :with_all_args }
+
+      it { expect(reagents_list_item_list_element_mapper).to be_a described_class }
+      it { expect(reagents_list_item_list_element_mapper).to be_a ShaleCustom::Mapper }
+      it { expect(reagents_list_item_list_element_mapper.type).to eq args[:type] }
+      it { expect(reagents_list_item_list_element_mapper.dct_conformsTo).to eq args[:dct_conformsTo] }
+      it { expect(reagents_list_item_list_element_mapper.id).to eq args[:id] }
+      it { expect(reagents_list_item_list_element_mapper.identifier).to eq args[:identifier] }
+      it { expect(reagents_list_item_list_element_mapper.name).to eq args[:name] }
+      it { expect(reagents_list_item_list_element_mapper.molecularFormula).to eq args[:molecularFormula] }
+      it { expect(reagents_list_item_list_element_mapper.inChIKey).to eq args[:inChIKey] }
+      it { expect(reagents_list_item_list_element_mapper.smiles).to eq args[:smiles] }
+    end
+
+    context "when called with some arguments" do
+      let(:args) { attributes_for :reagents_list_item_list_element_mapper, :with_all_args, type: nil, id: nil, molecularFormula: nil }
+      let(:reagents_list_item_list_element_mapper) { described_class.new(**args) }
+      let(:dct_list_mapper) { build :dct_list_mapper, :with_all_args }
+
+      it { expect(reagents_list_item_list_element_mapper).to be_a described_class }
+      it { expect(reagents_list_item_list_element_mapper).to be_a ShaleCustom::Mapper }
+      it { expect(reagents_list_item_list_element_mapper.type).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.dct_conformsTo).to eq args[:dct_conformsTo] }
+      it { expect(reagents_list_item_list_element_mapper.id).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.identifier).to eq args[:identifier] }
+      it { expect(reagents_list_item_list_element_mapper.name).to eq args[:name] }
+      it { expect(reagents_list_item_list_element_mapper.molecularFormula).to be_nil }
+      it { expect(reagents_list_item_list_element_mapper.inChIKey).to eq args[:inChIKey] }
+      it { expect(reagents_list_item_list_element_mapper.smiles).to eq args[:smiles] }
+    end
+  end
+
+  describe "to_json" do
+    context "when called without any arguments" do
+      let(:reagents_list_item_list_element_mapper) { build :reagents_list_item_list_element_mapper }
+
       let(:expected_json) do
         <<~JSON
           {
@@ -34,25 +74,12 @@ describe ReactionMappers::ReagentsList::ItemListElementMapper do
 
       it { expect(reagents_list_item_list_element_mapper.to_json).to eq_without_whitespace expected_json }
     end
-  end
 
-  context "when called with all arguments" do
-    let(:args) { attributes_for :reagents_list_item_list_element_mapper, :with_all_args }
-    let(:reagents_list_item_list_element_mapper) { described_class.new(**args) }
-    let(:dct_list_mapper) { build :dct_list_mapper, :with_all_args }
+    context "when called with all arguments" do
+      let(:args) { attributes_for :reagents_list_item_list_element_mapper, :with_all_args }
+      let(:reagents_list_item_list_element_mapper) { described_class.new(**args) }
+      let(:dct_list_mapper) { build :dct_list_mapper, :with_all_args }
 
-    it { expect(reagents_list_item_list_element_mapper).to be_a described_class }
-    it { expect(reagents_list_item_list_element_mapper).to be_a ShaleCustom::Mapper }
-    it { expect(reagents_list_item_list_element_mapper.type).to eq args[:type] }
-    it { expect(reagents_list_item_list_element_mapper.dct_conformsTo).to eq args[:dct_conformsTo] }
-    it { expect(reagents_list_item_list_element_mapper.id).to eq args[:id] }
-    it { expect(reagents_list_item_list_element_mapper.identifier).to eq args[:identifier] }
-    it { expect(reagents_list_item_list_element_mapper.name).to eq args[:name] }
-    it { expect(reagents_list_item_list_element_mapper.molecularFormula).to eq args[:molecularFormula] }
-    it { expect(reagents_list_item_list_element_mapper.inChIKey).to eq args[:inChIKey] }
-    it { expect(reagents_list_item_list_element_mapper.smiles).to eq args[:smiles] }
-
-    describe "#to_json" do
       let(:expected_json) do
         <<~JSON
           {
@@ -70,25 +97,12 @@ describe ReactionMappers::ReagentsList::ItemListElementMapper do
 
       it { expect(reagents_list_item_list_element_mapper.to_json).to eq_without_whitespace expected_json }
     end
-  end
 
-  context "when called with some arguments" do
-    let(:args) { attributes_for :reagents_list_item_list_element_mapper, :with_all_args, type: nil, id: nil, molecularFormula: nil }
-    let(:reagents_list_item_list_element_mapper) { described_class.new(**args) }
-    let(:dct_list_mapper) { build :dct_list_mapper, :with_all_args }
+    context "when called with some arguments" do
+      let(:args) { attributes_for :reagents_list_item_list_element_mapper, :with_all_args, type: nil, id: nil, molecularFormula: nil }
+      let(:reagents_list_item_list_element_mapper) { described_class.new(**args) }
+      let(:dct_list_mapper) { build :dct_list_mapper, :with_all_args }
 
-    it { expect(reagents_list_item_list_element_mapper).to be_a described_class }
-    it { expect(reagents_list_item_list_element_mapper).to be_a ShaleCustom::Mapper }
-    it { expect(reagents_list_item_list_element_mapper.type).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.dct_conformsTo).to eq args[:dct_conformsTo] }
-    it { expect(reagents_list_item_list_element_mapper.id).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.identifier).to eq args[:identifier] }
-    it { expect(reagents_list_item_list_element_mapper.name).to eq args[:name] }
-    it { expect(reagents_list_item_list_element_mapper.molecularFormula).to be_nil }
-    it { expect(reagents_list_item_list_element_mapper.inChIKey).to eq args[:inChIKey] }
-    it { expect(reagents_list_item_list_element_mapper.smiles).to eq args[:smiles] }
-
-    describe "#to_json" do
       let(:expected_json) do
         <<~JSON
           {
