268744496aa126ee03bf50f74bfe190f8dd72914
Christian Deme <christian.deme@cleanercode.de>
2022-11-29 12:35:56 +0100

    Refactor spec for Reaction model, add spec for it

--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -1,4 +1,6 @@
 describe Reaction do
+  let(:reaction) { create :reaction, :with_realistic_attributes }
+
   it_behaves_like "Publication"
 
   it { expect(described_class.sti_name).to eq "Reaction" }
@@ -33,44 +35,40 @@ describe Reaction do
     end
   end
 
-  describe "#chemotion_id" do
-    subject(:chemotion_id) { reaction.chemotion_id }
-
+  describe ".new" do
     let(:reaction) { create :reaction, :with_realistic_attributes, id: 1 }
 
-    it { is_expected.to eq "CRR-#{reaction.id}" }
-    it { is_expected.to eq "CRR-1" }
-  end
+    it { expect(reaction).to be_a described_class }
 
-  describe "#doi" do
-    subject(:doi) { reaction.doi }
+    describe "#chemotion_id" do
+      subject(:chemotion_id) { reaction.chemotion_id }
 
-    let(:reaction) { create :reaction, :with_realistic_attributes }
+      it { is_expected.to eq "CRR-#{reaction.id}" }
+      it { is_expected.to eq "CRR-1" }
+    end
 
-    it { is_expected.to eq "10.14272/reaction/SA-FUHFF-UHFFFADPSC-WITXFYCLPD-UHFFFADPSC-NUHFF-NUHFF-NUHFF-ZZZ" }
-  end
+    describe "#doi" do
+      subject(:doi) { reaction.doi }
 
-  describe "#temperature_user_text" do
-    subject(:temperature_user_text) { reaction.temperature_user_text }
+      it { is_expected.to eq "10.14272/reaction/SA-FUHFF-UHFFFADPSC-WITXFYCLPD-UHFFFADPSC-NUHFF-NUHFF-NUHFF-ZZZ" }
+    end
 
-    let(:reaction) { create :reaction, :with_realistic_attributes }
+    describe "#temperature_user_text" do
+      subject(:temperature_user_text) { reaction.temperature_user_text }
 
-    it { is_expected.to eq "118" }
-  end
+      it { is_expected.to eq "118" }
+    end
 
-  describe "#temperature_value_unit" do
-    subject(:temperature_value_unit) { reaction.temperature_value_unit }
+    describe "#temperature_value_unit" do
+      subject(:temperature_value_unit) { reaction.temperature_value_unit }
 
-    let(:reaction) { create :reaction, :with_realistic_attributes, id: 1 }
+      it { is_expected.to eq "°C" }
+    end
 
-    it { is_expected.to eq "°C" }
-  end
+    describe "#present_to_api" do
+      subject(:present_to_api) { reaction.present_to_api }
 
-  describe "#present_to_api" do
-    subject(:present_to_api) { reaction.present_to_api }
-
-    let(:reaction) { create :reaction, :with_realistic_attributes }
-
-    it { expect(present_to_api).to be_a RootMappers::ReactionMapper }
+      it { expect(present_to_api).to be_a RootMappers::ReactionMapper }
+    end
   end
 end
