dacff0f71254d3179d90e5346c7c6183433965b5
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-30 17:36:48 +0100

    Add private method for reaction.samples

--- app/adapters/reaction_adapter/reagents_list_adapter.rb
+++ app/adapters/reaction_adapter/reagents_list_adapter.rb
@@ -4,11 +4,13 @@ module ReactionAdapter
 
     def initialize(reaction) = @reaction = reaction
 
-    def numberOfItems = @number_of_items ||= @reaction.samples.count
+    def numberOfItems = @number_of_items ||= reaction_samples.size
 
     def itemListElement = @item_list_element ||= item_list_element_adapter_iterator.to_a
 
     private
+
+    def reaction_samples = @reaction_samples ||= @reaction.samples
 
     def item_list_element_adapter_iterator = @item_list_element_adapter_iterator ||= ReagentsList::ItemListElementAdapterIterator.new(@reaction)
   end
