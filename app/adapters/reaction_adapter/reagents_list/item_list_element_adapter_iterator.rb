d5cf68ddc9aceb0d088d59264c52357f2497e1b6
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-30 17:33:40 +0100

    Move ItemListElementAdapter creation to private method

--- app/adapters/reaction_adapter/reagents_list/item_list_element_adapter_iterator.rb
+++ app/adapters/reaction_adapter/reagents_list/item_list_element_adapter_iterator.rb
@@ -2,6 +2,10 @@ module ReactionAdapter::ReagentsList
   class ItemListElementAdapterIterator
     def initialize(reaction) = @reaction = reaction
 
-    def to_a = @to_a ||= @reaction.samples.map { |sample| ItemListElementAdapter.new(sample).to_h }
+    def to_a = @to_a ||= @reaction.samples.map { |sample| item_list_element_adapter(sample).to_h }
+
+    private
+
+    def item_list_element_adapter(sample) = ItemListElementAdapter.new(sample)
   end
 end
