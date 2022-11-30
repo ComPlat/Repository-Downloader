653f20be8e28fab1b42ec50ca9c8f3755feb42e4
Philipp Herda <philipp.herda@cleanercode.de>
2022-11-30 12:18:52 +0100

    Rename DctConformsAdapter to DctConformsToAdapter

--- spec/adapters/sample_adapter/dct_conforms_to_adapter_spec.rb
+++ spec/adapters/sample_adapter/dct_conforms_to_adapter_spec.rb
@@ -1,4 +1,4 @@
-describe SampleAdapter::DctConformsAdapter do
+describe SampleAdapter::DctConformsToAdapter do
   let(:dct_conforms_adapter) { described_class.new }
 
   describe ".new" do
@@ -10,6 +10,6 @@ describe SampleAdapter::DctConformsAdapter do
   describe "#to_h" do
     subject { dct_conforms_adapter.to_h }
 
-    it { is_expected.to eq({dct_conformsTo: SampleAdapter::DctElementAdapter.new.to_h}) }
+    it { is_expected.to eq({dct_conformsTo: SampleAdapter::DctConformsToElementAdapter.new.to_h}) }
   end
 end
