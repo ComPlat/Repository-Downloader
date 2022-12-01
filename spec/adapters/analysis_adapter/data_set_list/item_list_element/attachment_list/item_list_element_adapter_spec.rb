521c9819e3694d31668c7d32c275c50be9136c46
Philipp Herda <philipp.herda@cleanercode.de>
2022-12-01 16:11:51 +0100

    Rename variable

--- spec/adapters/analysis_adapter/data_set_list/item_list_element/attachment_list/item_list_element_adapter_spec.rb
+++ spec/adapters/analysis_adapter/data_set_list/item_list_element/attachment_list/item_list_element_adapter_spec.rb
@@ -1,27 +1,27 @@
 describe AnalysisAdapter::DataSetList::ItemListElement::AttachmentList::ItemListElementAdapter do
   let(:analysis) { create :analysis, :with_realistic_attributes, element_id: 1, attachments: [attachment] }
   let(:attachment) { build :attachment, :with_realistic_attributes }
-  let(:attachment_list_adapter) { described_class.new analysis, attachment }
+  let(:item_list_element_adapter) { described_class.new analysis, attachment }
 
   before do
     attachment
   end
 
   describe ".new" do
-    subject { attachment_list_adapter }
+    subject { item_list_element_adapter }
 
     it { is_expected.to be_a described_class }
   end
 
   describe "#to_h" do
-    subject { attachment_list_adapter.to_h }
+    subject { item_list_element_adapter.to_h }
 
     let(:expected_hash) do
       {
-        filename: attachment_list_adapter.filename,
-        filepath: attachment_list_adapter.filepath,
-        identifier: attachment_list_adapter.identifier,
-        type: attachment_list_adapter.type
+        filename: item_list_element_adapter.filename,
+        filepath: item_list_element_adapter.filepath,
+        identifier: item_list_element_adapter.identifier,
+        type: item_list_element_adapter.type
       }
     end
 
@@ -29,28 +29,28 @@ describe AnalysisAdapter::DataSetList::ItemListElement::AttachmentList::ItemList
   end
 
   describe "#type" do
-    subject { attachment_list_adapter.type }
+    subject { item_list_element_adapter.type }
 
     it { is_expected.to eq "AttachmentEntity" }
     it { is_expected.to be_a String }
   end
 
   describe "#identifier" do
-    subject { attachment_list_adapter.identifier }
+    subject { item_list_element_adapter.identifier }
 
     it { is_expected.to eq attachment.identifier }
     it { is_expected.to be_a String }
   end
 
   describe "#filename" do
-    subject { attachment_list_adapter.filename }
+    subject { item_list_element_adapter.filename }
 
     it { is_expected.to eq analysis.attachments.first.filename }
     it { is_expected.to be_a String }
   end
 
   describe "#filepath" do
-    subject { attachment_list_adapter.filepath }
+    subject { item_list_element_adapter.filepath }
 
     let(:expected_file_path) { "data/#{analysis.chemotion_id}" }
 
