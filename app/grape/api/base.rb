0d59b480a7f397add7d200b98e6b6731b099c006
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-02 16:43:04 +0100

    Adding TODO comments

--- app/grape/api/base.rb
+++ app/grape/api/base.rb
@@ -15,6 +15,8 @@ module API
     content_type :csv, "text/csv"
     formatter :csv, ->(object, _env) { object.to_csv }
 
+    # TODO: Add content_type :zip
+
     # HINT: Needed to avoid CORS (Cross-Origin Resource Sharing) error.
     #       Swagger UI returns response code 0 if these lines aren't here.
     before do
