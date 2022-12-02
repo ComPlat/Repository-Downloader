58c73f36ae8a6ca68a82e06ac82e69a905bc2887
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-02 15:13:56 +0100

    Removed not needed empty line.

--- app/presenters/publication_presenter.rb
+++ app/presenters/publication_presenter.rb
@@ -3,6 +3,5 @@ class PublicationPresenter
   def self.present_by_doi(doi) = PublicationRepository.find_by_doi!(doi).present_to_api
 
   def self.present_by_chemotion_id(chemotion_id) = PublicationRepository.find_by_chemotion_id!(chemotion_id).present_to_api
-
   # rubocop:enable Rails/DynamicFindBy
 end
