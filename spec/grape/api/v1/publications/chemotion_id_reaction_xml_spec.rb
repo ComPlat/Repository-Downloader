e048bfb6adbd88f1f1d74250f284a277fe7df6cc
Philipp Herda <philipp.herda@cleanercode.de>
2022-12-04 00:36:14 +0100

    Implement reaction xml feature specs

--- spec/grape/api/v1/publications/chemotion_id_reaction_xml_spec.rb
+++ spec/grape/api/v1/publications/chemotion_id_reaction_xml_spec.rb
@@ -1,2 +1,172 @@
 describe API::V1::Publications::ChemotionId, ".reaction" do
+  context "when one reaction and no attachments" do
+    let(:reaction) { create :reaction, :with_realistic_attributes }
+
+    let(:expected_xml) do
+      <<~XML
+        <reaction>
+          <context>https://schema.org/</context>
+          <id>10.14272/reaction/SA-FUHFF-UHFFFADPSC-WITXFYCLPD-UHFFFADPSC-NUHFF-NUHFF-NUHFF-ZZZ</id>
+          <type>BioChemicalReaction</type>
+          <name/>
+          <identifier>CRR-#{reaction.id}</identifier>
+          <status>Successful</status>
+          <description>--- !ruby/hash:Hashie::Mashops:- !ruby/hash:Hashie::Mash
+                    insert: '2-Benzofuran-1,3-dione (75.0 g, 506 mmol, 1.00 equiv) and phenylmethanamine
+                      (63.9 g, 65.0 mL, 596 mmol, 1.18 equiv) were dissolved in 300 mL glacial acetic
+                      acid and refluxed for 4 h. After cooling to room temperature, 700 mL of water
+                      were added. The precipitate was filtered off and washed with water. The crude
+                      product was recrystallized from ethanol to give 106 g of a colorless solid.
+                  
+                  '
+          </description>
+          <temperature>118 &#xB0;C</temperature>
+          <reaction_type>MOP:0000790 | substitution reaction</reaction_type>
+          <duration>4 Hour(s)</duration>
+          <purification>Crystallisation</purification>
+          <reagents_list>
+            <numberOfItems>0</numberOfItems>
+          </reagents_list>
+        </reaction>
+      XML
+    end
+
+    before { get "/api/v1/publications/chemotion_id/#{reaction.id}.xml" }
+
+    it { expect(response.parsed_body).to eq_without_whitespace expected_xml }
+  end
+
+  context "when one reaction and one attachment" do
+    let(:reaction) { create :reaction, :with_realistic_attributes }
+    let(:attached_sample) { create :sample, :with_realistic_attributes, reaction:, id: 2 }
+
+    let(:expected_reagents_list_xml) do
+      <<~XML
+        <numberOfItems>1</numberOfItems>
+        <itemListElement>
+          <type>MolecularEntity</type>
+          <dct_conformsTo>
+            <type>CreativeWork</type>
+            <id>https://bioschemas.org/profiles/MolecularEntity/0.5-RELEASE/</id>
+          </dct_conformsTo>
+          <id>10.14272/MUAMZYSBUQADBN-UHFFFAOYSA-N.1</id>
+          <identifier>CRS-#{attached_sample.id}</identifier>
+          <name>2-[5-[3-(5-pyridin-2-yl-2H-triazol-4-yl)phenyl]-2H-triazol-4-yl]pyridine</name>
+          <molecularFormula>C20H14N8</molecularFormula>
+          <inChIKey>MUAMZYSBUQADBN-UHFFFAOYSA-N</inChIKey>
+          <smiles>c1ccc(nc1)c1[nH]nnc1c1cccc(c1)c1[nH]nnc1c1ccccn1</smiles>
+        </itemListElement>
+      XML
+    end
+    let(:expected_xml) do
+      <<~XML
+        <reaction>
+          <context>https://schema.org/</context>
+          <id>10.14272/reaction/SA-FUHFF-UHFFFADPSC-WITXFYCLPD-UHFFFADPSC-NUHFF-NUHFF-NUHFF-ZZZ</id>
+          <type>BioChemicalReaction</type>
+          <name>2-[5-[3-(5-pyridin-2-yl-2H-triazol-4-yl)phenyl]-2H-triazol-4-yl]pyridine</name>
+          <identifier>CRR-#{reaction.id}</identifier>
+          <status>Successful</status>
+          <description>--- !ruby/hash:Hashie::Mashops:- !ruby/hash:Hashie::Mash
+                    insert: '2-Benzofuran-1,3-dione (75.0 g, 506 mmol, 1.00 equiv) and phenylmethanamine
+                      (63.9 g, 65.0 mL, 596 mmol, 1.18 equiv) were dissolved in 300 mL glacial acetic
+                      acid and refluxed for 4 h. After cooling to room temperature, 700 mL of water
+                      were added. The precipitate was filtered off and washed with water. The crude
+                      product was recrystallized from ethanol to give 106 g of a colorless solid.
+                  
+                  '
+          </description>
+          <temperature>118 &#xB0;C</temperature>
+          <reaction_type>MOP:0000790 | substitution reaction</reaction_type>
+          <duration>4 Hour(s)</duration>
+          <purification>Crystallisation</purification>
+          <reagents_list>
+            #{expected_reagents_list_xml}
+          </reagents_list>
+        </reaction>
+      XML
+    end
+
+    before do
+      attached_sample
+      get "/api/v1/publications/chemotion_id/#{reaction.id}.xml"
+    end
+
+    it { expect(response.parsed_body).to eq_without_whitespace expected_xml }
+  end
+
+  context "when one reaction and two attachments" do
+    let(:reaction) { create :reaction, :with_realistic_attributes }
+    let(:attachted_samples) {
+      [(create :sample, :with_realistic_attributes, reaction:, id: 2),
+        (create :sample, :with_realistic_attributes, reaction:, id: 3)]
+    }
+
+    let(:expected_reagents_list_xml) do
+      <<~XML
+        <numberOfItems>2</numberOfItems>
+        <itemListElement>
+          <type>MolecularEntity</type>
+          <dct_conformsTo>
+            <type>CreativeWork</type>
+            <id>https://bioschemas.org/profiles/MolecularEntity/0.5-RELEASE/</id>
+          </dct_conformsTo>
+        <id>10.14272/MUAMZYSBUQADBN-UHFFFAOYSA-N.1</id>
+        <identifier>CRS-#{attachted_samples.first.id}</identifier>
+        <name>2-[5-[3-(5-pyridin-2-yl-2H-triazol-4-yl)phenyl]-2H-triazol-4-yl]pyridine</name>
+        <molecularFormula>C20H14N8</molecularFormula>
+        <inChIKey>MUAMZYSBUQADBN-UHFFFAOYSA-N</inChIKey>
+        <smiles>c1ccc(nc1)c1[nH]nnc1c1cccc(c1)c1[nH]nnc1c1ccccn1</smiles>
+        </itemListElement>
+        <itemListElement>
+          <type>MolecularEntity</type>
+          <dct_conformsTo>
+            <type>CreativeWork</type>
+            <id>https://bioschemas.org/profiles/MolecularEntity/0.5-RELEASE/</id>
+          </dct_conformsTo>
+          <id>10.14272/MUAMZYSBUQADBN-UHFFFAOYSA-N.1</id>
+          <identifier>CRS-#{attachted_samples.second.id}</identifier>
+          <name>2-[5-[3-(5-pyridin-2-yl-2H-triazol-4-yl)phenyl]-2H-triazol-4-yl]pyridine</name>
+          <molecularFormula>C20H14N8</molecularFormula>
+          <inChIKey>MUAMZYSBUQADBN-UHFFFAOYSA-N</inChIKey>
+          <smiles>c1ccc(nc1)c1[nH]nnc1c1cccc(c1)c1[nH]nnc1c1ccccn1</smiles>
+        </itemListElement>
+      XML
+    end
+    let(:expected_xml) do
+      <<~XML
+        <reaction>
+          <context>https://schema.org/</context>
+          <id>10.14272/reaction/SA-FUHFF-UHFFFADPSC-WITXFYCLPD-UHFFFADPSC-NUHFF-NUHFF-NUHFF-ZZZ</id>
+          <type>BioChemicalReaction</type>
+          <name>2-[5-[3-(5-pyridin-2-yl-2H-triazol-4-yl)phenyl]-2H-triazol-4-yl]pyridine 2-[5-[3-(5-pyridin-2-yl-2H-triazol-4-yl)phenyl]-2H-triazol-4-yl]pyridine</name>
+          <identifier>CRR-#{reaction.id}</identifier>
+          <status>Successful</status>
+          <description>--- !ruby/hash:Hashie::Mashops:- !ruby/hash:Hashie::Mash
+                    insert: '2-Benzofuran-1,3-dione (75.0 g, 506 mmol, 1.00 equiv) and phenylmethanamine
+                      (63.9 g, 65.0 mL, 596 mmol, 1.18 equiv) were dissolved in 300 mL glacial acetic
+                      acid and refluxed for 4 h. After cooling to room temperature, 700 mL of water
+                      were added. The precipitate was filtered off and washed with water. The crude
+                      product was recrystallized from ethanol to give 106 g of a colorless solid.
+                  
+                  '
+          </description>
+          <temperature>118 &#xB0;C</temperature>
+          <reaction_type>MOP:0000790 | substitution reaction</reaction_type>
+          <duration>4 Hour(s)</duration>
+          <purification>Crystallisation</purification>
+          <reagents_list>
+            #{expected_reagents_list_xml}
+          </reagents_list>
+        </reaction>
+      XML
+    end
+
+    before do
+      attachted_samples
+      get "/api/v1/publications/chemotion_id/#{reaction.id}.xml"
+    end
+
+    it { expect(response.parsed_body).to eq_without_whitespace expected_xml }
+  end
 end
