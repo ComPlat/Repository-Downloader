37cef478ebcae1969a583d9951a7bfb98ba4cd95
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-07 17:51:51 +0100

    Improvements on Endpoint for Swagger

--- app/grape/api/v1/publications.rb
+++ app/grape/api/v1/publications.rb
@@ -4,17 +4,24 @@ class API::V1::Publications < Grape::API
   namespace "/publications" do
     desc "Get publications via ChemotionID(s) or DOI(s)", {produces: %w[application/json application/xml text/csv application/zip]}
     params do
-      optional :dois, type: Types::ValidDoiSet, desc: "List of DOIs, separated by comma"
-      optional :chemotion_ids, type: Types::PositiveIntegerSet, desc: "List of Chemotion IDs, separated by comma"
-      requires :format, type: Types::SupportedRenderingFormat, desc: "Rendering Format"
+      optional :dois,
+        type: Array[String],
+        coerce_with: ->(value) { Types::ValidDoiSet.parse(value).value },
+        documentation: {collectionFormat: "csv", param_type: "query"},
+        desc: "List of DOIs, separated by comma, either this or chemotion_ids needs to be given"
+      optional :chemotion_ids,
+        type: Array[Integer],
+        coerce_with: ->(value) { Types::PositiveIntegerSet.parse(value).value },
+        documentation: {collectionFormat: "csv", param_type: "query"},
+        desc: "List of Chemotion IDs, separated by comma, either this or dois needs to be given"
       exactly_one_of :chemotion_ids, :dois
     end
     get do
       case params
-      in {chemotion_ids: Types::PositiveIntegerSet}
-        stream PublicationsByChemotionIdPresenter.new(params[:chemotion_ids].value).public_send("to_#{params[:format].value}")
-      in {dois: Types::ValidDoiSet}
-        stream PublicationsByDoiPresenter.new(params[:dois].value).public_send("to_#{params[:format].value}")
+      in {chemotion_ids: Array}
+        stream PublicationsByChemotionIdPresenter.new(params[:chemotion_ids]).public_send("to_#{env["api.format"]}")
+      in {dois: Array}
+        stream PublicationsByDoiPresenter.new(params[:dois].value).public_send("to_#{env["api.format"]}")
       else
         error!("The server does not support the functionality required to fulfill the request.", 501)
       end
