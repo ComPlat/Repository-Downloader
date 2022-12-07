82b47de92aec1863d4e4202a41103fae70667435
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-07 14:39:17 +0100

    Refactored API::V1::Publications::ChemotionId

--- app/grape/api/v1/publications/chemotion_id.rb
+++ app/grape/api/v1/publications/chemotion_id.rb
@@ -1,22 +1,18 @@
-module API
-  module V1
-    module Publications
-      class ChemotionId < Grape::API
-        version "v1", using: :path
+module API::V1::Publications
+  class ChemotionId < Grape::API
+    version "v1", using: :path
 
-        namespace "/publications/chemotion_id" do
-          desc "Get publication(s) via ChemotionID(s)", {produces: %w[application/json application/xml text/csv application/zip]}
-          params do
-            requires :ids, type: Types::PositiveIntegerSet, desc: "ChemotionID"
-            requires :format, type: Types::SupportedRenderingFormat, desc: "Rendering Format"
-          end
-          get do
-            stream PublicationsByChemotionIdPresenter.new(params[:ids].value).public_send("to_#{params[:format].value}")
-          end
-        end
-
-        route(:any, "*path") { error!("Not found", 404) }
+    namespace "/publications/chemotion_id" do
+      desc "Get publication(s) via ChemotionID(s)", {produces: %w[application/json application/xml text/csv application/zip]}
+      params do
+        requires :ids, type: Types::PositiveIntegerSet, desc: "ChemotionID"
+        requires :format, type: Types::SupportedRenderingFormat, desc: "Rendering Format"
+      end
+      get do
+        stream PublicationsByChemotionIdPresenter.new(params[:ids].value).public_send("to_#{params[:format].value}")
       end
     end
+
+    route(:any, "*path") { error!("Not found", 404) }
   end
 end
