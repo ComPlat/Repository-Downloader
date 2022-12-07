7ec68061b022b3f781960e6a835defd0e449e171
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-07 14:32:49 +0100

    Removed not needed PublicationsPresenter

--- app/presenters/publications_presenter.rb
+++
@@ -1,7 +0,0 @@
-class PublicationsPresenter
-  def self.present_by_dois(dois) = dois.map { |doi| PublicationPresenter.present_by_doi doi }
-
-  def self.present_by_chemotion_ids(chemotion_ids) = NewMappersPresenter.new(Publication.where(id: chemotion_ids))
-
-  # rubocop:enable Rails/DynamicFindBy
-end
