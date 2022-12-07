feb8d3d8e31d92778ecde9172dcdb13fe1f18190
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-07 19:41:49 +0100

    Bugfix and format for API::V1::Publications

--- app/grape/api/v1/publications.rb
+++ app/grape/api/v1/publications.rb
@@ -2,7 +2,8 @@ class API::V1::Publications < Grape::API
   version "v1", using: :path
 
   namespace "/publications" do
-    desc "Get publications via ChemotionID(s) or DOI(s)", {produces: %w[application/json application/xml text/csv application/zip]}
+    desc "Get publications via ChemotionID(s) or DOI(s)", produces: %w[application/json application/xml text/csv application/zip]
+
     params do
       optional :dois,
         type: Array[String],
@@ -16,12 +17,13 @@ class API::V1::Publications < Grape::API
         desc: "List of Chemotion IDs, separated by comma, either this or dois needs to be given"
       exactly_one_of :chemotion_ids, :dois
     end
+
     get do
       case params
       in {chemotion_ids: Array}
         stream PublicationsByChemotionIdPresenter.new(params[:chemotion_ids]).public_send("to_#{env["api.format"]}")
       in {dois: Array}
-        stream PublicationsByDoiPresenter.new(params[:dois].value).public_send("to_#{env["api.format"]}")
+        stream PublicationsByDoiPresenter.new(params[:dois]).public_send("to_#{env["api.format"]}")
       else
         error!("The server does not support the functionality required to fulfill the request.", 501)
       end
