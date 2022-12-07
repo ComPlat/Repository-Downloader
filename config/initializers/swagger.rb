c49a921d6a740e06d7aeff1ebc3af7cd55f6f1f8
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-07 19:42:17 +0100

    Some config for GrapeSwaggerRails

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -2,3 +2,5 @@
 GrapeSwaggerRails.options.url = "api/swagger_doc.json"
 # noinspection RubyResolve
 GrapeSwaggerRails.options.app_url = ENV["SWAGGER_UI"]
+# noinspection RubyResolve
+GrapeSwaggerRails.options.doc_expansion = "full"
