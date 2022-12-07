bf638b83dc3e194b34ffd0e8dfccce294b901358
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-07 14:34:59 +0100

    Refactored API::V1::Publications::Doi

--- app/grape/api/v1/publications/doi.rb
+++ app/grape/api/v1/publications/doi.rb
@@ -1,22 +1,18 @@
-module API
-  module V1
-    module Publications
-      class Doi < Grape::API
-        version "v1", using: :path
+module API::V1::Publications
+  class Doi < Grape::API
+    version "v1", using: :path
 
-        namespace "/publications/doi" do
-          desc "Get one publication via Doi", {produces: %w[application/json application/xml text/csv]}
-          params do
-            requires :dois, type: Types::ValidDoiSet, desc: "Doi"
-            requires :format, type: Types::SupportedRenderingFormat, desc: "Rendering Format"
-          end
-          get do
-            stream PublicationsByDoiPresenter.new(params[:dois].value).public_send("to_#{params[:format].value}")
-          end
-        end
-
-        route(:any, "*path") { error!("Not found", 404) }
+    namespace "/publications/doi" do
+      desc "Get one publication via Doi", {produces: %w[application/json application/xml text/csv]}
+      params do
+        requires :dois, type: Types::ValidDoiSet, desc: "Doi"
+        requires :format, type: Types::SupportedRenderingFormat, desc: "Rendering Format"
+      end
+      get do
+        stream PublicationsByDoiPresenter.new(params[:dois].value).public_send("to_#{params[:format].value}")
       end
     end
+
+    route(:any, "*path") { error!("Not found", 404) }
   end
 end
