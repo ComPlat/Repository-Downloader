7c6f585be85f6928ba61321c517e278dfff7c519
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-08 16:37:34 +0100

    fixing url for Swagger UI in production by using relative url

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import SwaggerUI from 'swagger-ui-react';
 
 const App = (): JSX.Element => {
-  return <SwaggerUI url='http://localhost:3000/api/swagger_doc.json' />;
+  return <SwaggerUI url='/api/swagger_doc.json' />;
 };
 
 ReactDOM.render(<App />, document.querySelector('#spa'));
