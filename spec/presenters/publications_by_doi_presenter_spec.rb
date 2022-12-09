0e5c78c03d0c88eeeec300dc1a7b18883c0e772a
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-09 23:48:25 +0100

    Adding missing spec for PublicationsByDoiPresenter.new and PublicationsByDoiPresenter#to_json

---
+++ spec/presenters/publications_by_doi_presenter_spec.rb
@@ -0,0 +1,34 @@
+describe PublicationsByDoiPresenter do
+  describe ".new" do
+    subject(:new) { described_class.new [] }
+
+    it { is_expected.to be_a described_class }
+  end
+
+  describe "#to_json" do
+    subject(:to_json) { publication_by_doi_presenter.to_json }
+
+    let(:publication_by_doi_presenter) { described_class.new dois }
+
+    context "when dois is an empty array" do
+      let(:dois) { [] }
+
+      it { expect(to_json.to_a.join).to eq "[]" }
+    end
+
+    context "when dois is an array with one doi in it" do
+      let(:analysis) { create(:analysis, :with_realistic_attributes) }
+      let(:dois) { [analysis.doi] }
+
+      it { expect(to_json.to_a.join).to eq analysis.present_to_api.to_json.to_a.join }
+    end
+
+    context "when dois is an array with two dois in it" do
+      let(:analysis1) { create(:analysis, :with_realistic_attributes) }
+      let(:analysis2) { create(:analysis, :with_realistic_attributes, taggable_data: {"doi" => analysis1.doi.tr("N", "Z")}) }
+      let(:dois) { [analysis1.doi, analysis2.doi] }
+
+      it { expect(to_json.to_a.join).to eq "[#{analysis1.present_to_api.to_json.to_a.join},#{analysis1.present_to_api.to_json.to_a.join}]" }
+    end
+  end
+end
