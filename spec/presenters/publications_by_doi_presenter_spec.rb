e6245003d644d670c50f7d3499d0ba3de6d730c8
Lukas Engel <79928782+Respicite@users.noreply.github.com>
2022-12-11 12:21:08 +0100

    Added / Extended Specs for publications_by_chemotion_id_presenter_spec.rb and publications_by_doi_presenter_spec.rb

--- spec/presenters/publications_by_doi_presenter_spec.rb
+++ spec/presenters/publications_by_doi_presenter_spec.rb
@@ -23,19 +23,56 @@ describe PublicationsByDoiPresenter do
       it { expect(to_json.to_a.join).to eq "[#{analysis.present_to_api.to_json.to_a.join}]" }
     end
 
-    context "when dois is an array with two dois in it" do
+    context "when dois is an array with two doi in it" do
       let(:analysis1) { create(:analysis, :with_realistic_attributes) }
       let(:analysis2) do
-        create(:analysis, :with_realistic_attributes, taggable_data: {"doi" => analysis1.doi.tr("N", "Z"),
-                                                                      "analysis_doi" => analysis1.doi.tr("N", "Z")})
+        create(:analysis, :with_realistic_attributes, taggable_data: {"doi" => analysis1.doi.tr("N", "X"),
+                                                                      "analysis_doi" => analysis1.doi.tr("N", "X")})
       end
       let(:dois) { [analysis1.doi, analysis2.doi] }
 
-      let(:present_to_api) do
+      let(:expected_json) do
         "[#{analysis1.present_to_api.to_json.to_a.join},#{analysis2.present_to_api.to_json.to_a.join}]"
       end
 
-      it { expect(to_json.to_a.join).to eq present_to_api }
+      it { expect(to_json.to_a.join).to eq expected_json }
+    end
+  end
+
+  describe "#to_xml" do
+    subject(:to_xml) { publication_by_doi_presenter.to_xml }
+
+    let(:publication_by_doi_presenter) { described_class.new dois }
+
+    context "when dois is an empty array" do
+      let(:dois) { [] }
+
+      it { expect(to_xml.to_a.join).to eq "<publications></publications>" }
+    end
+
+    context "when dois is an array with one doi in it" do
+      let(:analysis) { create(:analysis, :with_realistic_attributes) }
+      let(:dois) { [analysis.doi] }
+
+      it { expect(to_xml.to_a.join).to eq "<publications>#{analysis.present_to_api.to_xml.to_a.join}</publications>" }
+    end
+
+    context "when dois is an array with two doi in it" do
+      let(:analysis1) { create(:analysis, :with_realistic_attributes) }
+      let(:analysis2) do
+        create(:analysis, :with_realistic_attributes, taggable_data: {"doi" => analysis1.doi.tr("N", "A"),
+                                                                      "analysis_doi" => analysis1.doi.tr("N", "A")})
+      end
+      let(:dois) { [analysis1.doi, analysis2.doi] }
+
+      let(:expected_xml) do
+        ["<publications>",
+          analysis1.present_to_api.to_xml.to_a.join,
+          analysis2.present_to_api.to_xml.to_a.join,
+          "</publications>"].join
+      end
+
+      it { expect(to_xml.to_a.join).to eq expected_xml }
     end
   end
 end
