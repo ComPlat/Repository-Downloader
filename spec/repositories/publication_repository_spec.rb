a39a818f1665a1b0f9164e3e2e85e36426888569
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-12 15:12:15 +0100

    Refactor spec for PublicationRepository

--- spec/repositories/publication_repository_spec.rb
+++ spec/repositories/publication_repository_spec.rb
@@ -51,60 +51,38 @@ describe PublicationRepository do
   describe ".where_dois" do
     subject(:where_dois) { described_class.where_dois dois }
 
-    context "when one Analysis can be found" do
-      before {
-        analysis2
-        reaction1
-        reaction2
-      }
+    before {
+      analysis1
+      analysis2
+      reaction1
+      reaction2
+    }
 
+    context "when one Analysis can be found" do
       let(:dois) { [analysis1.doi] }
 
       it { is_expected.to match_array [analysis1] }
     end
 
     context "when one Reaction can be found" do
-      before {
-        analysis1
-        analysis2
-        reaction2
-      }
-
       let(:dois) { [reaction1.doi] }
 
       it { is_expected.to match_array [reaction1] }
     end
 
-    context "when one Reaction can be found" do
-      before {
-        analysis1
-        analysis2
-      }
-
+    context "when two Reactions can be found" do
       let(:dois) { [reaction1.doi, reaction2.doi] }
 
       it { is_expected.to match_array [reaction1, reaction2] }
     end
 
-    context "when two Analysises can be found" do
-      before {
-        reaction1
-        reaction2
-      }
-
+    context "when two Analyses can be found" do
       let(:dois) { [analysis1.doi, analysis2.doi] }
 
       it { is_expected.to match_array [analysis1, analysis2] }
     end
 
     context "when NO Publication can be found" do
-      before {
-        analysis1
-        analysis2
-        reaction1
-        reaction2
-      }
-
       let(:dois) { ["not_a_doi"] }
 
       it { is_expected.to match_array [] }
