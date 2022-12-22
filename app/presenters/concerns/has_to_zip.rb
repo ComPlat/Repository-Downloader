cd8cb1eb7c88acbf3ff83ba4f092618b0baa3dbd
Tobias Vetter <tobias.vetter@mailbox.org>
2022-12-22 15:57:56 +0100

    Using endless methods

--- app/presenters/concerns/has_to_zip.rb
+++ app/presenters/concerns/has_to_zip.rb
@@ -3,16 +3,12 @@
 module HasToZip
   extend ActiveSupport::Concern
 
-  def to_zip
-    BagItStream.new(bag_it_stream_args).rack_body
-  end
+  def to_zip = BagItStream.new(bag_it_stream_args).rack_body
 
   private
 
-  def bag_it_stream_args
-    # HINT: [ { files: { "file_name1" => "file_path1", "file_name2" => StringIO.new("file_content2") },
-    #         bag_path: "path_to_bag_does_not_need_to_exists_yet_but_if_it_does_clean_it_up_yourself",
-    #         cache_bag: true} ]
-    publications.map { |publication| BagItStreamArgBuilder.new(self, publication).build }
-  end
+  # HINT: [ { files: { "file_name1" => "file_path1", "file_name2" => StringIO.new("file_content2") },
+  #         bag_path: "path_to_bag_does_not_need_to_exists_yet_but_if_it_does_clean_it_up_yourself",
+  #         cache_bag: true} ]
+  def bag_it_stream_args = publications.map { |publication| BagItStreamArgBuilder.new(self, publication).build }
 end
