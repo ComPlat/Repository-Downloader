98d4a1134b470ae5bb6f5124796c5c6472d3c2a3
Philipp Herda <philipp.herda@cleanercode.de>
2022-12-27 16:51:36 +0100

    Add positive element_type filtering for chemotion_ids search

--- app/repositories/publication_repository.rb
+++ app/repositories/publication_repository.rb
@@ -1,9 +1,10 @@
 class PublicationRepository
   DOI_FIELD_NAMES = [:doi, :analysis_doi].freeze
   JSONB_QUERY = "taggable_data @> any(array[?]::jsonb[])".freeze
+  ELEMENT_TYPES = %w[Container Reaction Sample]
 
   def self.where_dois(dois) = Publication.where JSONB_QUERY, DOI_FIELD_NAMES.product(dois)
     .map { |field_name_doi_pair| {field_name_doi_pair.first => field_name_doi_pair.second}.to_json }
 
-  def self.where_chemotion_ids(chemotion_ids) = Publication.where(id: chemotion_ids)
+  def self.where_chemotion_ids(chemotion_ids) = Publication.where(id: chemotion_ids, element_type: ELEMENT_TYPES)
 end
