e49a2bd2c5d17b78fc0a64a02382c6282dc4872c
Philipp Herda <philipp.herda@cleanercode.de>
2022-12-30 14:23:33 +0100

    Add element_type filter for where_dois method

--- app/repositories/publication_repository.rb
+++ app/repositories/publication_repository.rb
@@ -3,8 +3,11 @@ class PublicationRepository
   JSONB_QUERY = "taggable_data @> any(array[?]::jsonb[])".freeze
   ELEMENT_TYPES = %w[Container Reaction Sample]
 
-  def self.where_dois(dois) = Publication.where JSONB_QUERY, DOI_FIELD_NAMES.product(dois)
-    .map { |field_name_doi_pair| {field_name_doi_pair.first => field_name_doi_pair.second}.to_json }
+  def self.where_dois(dois) = Publication.where(element_type: ELEMENT_TYPES)
+    .where(JSONB_QUERY, DOI_FIELD_NAMES.product(dois)
+                                                                            .map { |field_name_doi_pair|
+                                                                              {field_name_doi_pair.first => field_name_doi_pair.second}.to_json
+                                                                            })
 
   def self.where_chemotion_ids(chemotion_ids) = Publication.where(id: chemotion_ids, element_type: ELEMENT_TYPES)
 end
