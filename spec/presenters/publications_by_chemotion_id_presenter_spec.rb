987b3253501d04222ded063ee7eda71d746bd8bf
Philipp Herda <philipp.herda@cleanercode.de>
2023-01-03 12:37:30 +0100

    Add spec for multiple attachments

--- spec/presenters/publications_by_chemotion_id_presenter_spec.rb
+++ spec/presenters/publications_by_chemotion_id_presenter_spec.rb
@@ -182,7 +182,7 @@ describe PublicationsByChemotionIdPresenter do
       before do
         attachment
 
-        FileUtils.mkpath "#{analysis.attachments.first.bucket}"
+        FileUtils.mkpath analysis.attachments.first.bucket.to_s
         FileUtils.cp "./spec/support/cleanercode.png", "#{analysis.attachments.first.bucket}/#{analysis.attachments.first.identifier}"
 
         zip_chunks = []
@@ -204,7 +204,79 @@ describe PublicationsByChemotionIdPresenter do
 
       it do
         expect(File.size("#{analysis.attachments.first.bucket}/#{analysis.attachments.first.identifier}"))
-          .to eq File.size("./spec/support/cleanercode.png")
+          .to eq File.size(Dir.glob("./tmp/output/*").find { |folder| File.directory?(folder) } + "/data/#{attachment.filename}")
+      end
+
+      it do
+        expect(File.read(Dir.glob("./tmp/output/*")
+                            .find { |folder| File.directory?(folder) } + "/data/analysis.json"))
+          .to eq analysis.present_to_api.to_json.to_a.join
+      end
+
+      it do
+        expect(File.read(Dir.glob("./tmp/output/*")
+                            .find { |folder| File.directory?(folder) } + "/data/analysis.xml"))
+          .to eq analysis.present_to_api.to_xml.to_a.join
+      end
+    end
+
+    context "when chemotion_ids is an array with one chemotion_id in it and analysis has 5 attachments" do
+      let(:analysis) { create(:analysis, :with_realistic_attributes, element_id: 1, attachments: attachments.map { |attachment| attachment }) }
+      let(:attachments) {
+        [build(:attachment, :with_realistic_attributes),
+          build(:attachment, :with_realistic_attributes, filename: "JK21-proton.peak.png"),
+          build(:attachment, :with_realistic_attributes, filename: "JK22-proton.peak.png"),
+          build(:attachment, :with_realistic_attributes, filename: "JK23-proton.peak.png"),
+          build(:attachment, :with_realistic_attributes, filename: "JK24-proton.peak.png")]
+      }
+      let(:chemotion_ids) { [analysis.id] }
+
+      before do
+        attachments
+
+        FileUtils.mkpath analysis.attachments.first.bucket.to_s
+        FileUtils.cp "./spec/support/cleanercode.png", "#{analysis.attachments.first.bucket}/#{analysis.attachments.first.identifier}"
+
+        zip_chunks = []
+        to_zip.each { |zip_chunk|
+          zip_chunks << zip_chunk
+        }
+
+        unzip(io: StringIO.new(zip_chunks.join))
+      end
+
+      after do
+        FileUtils.rm_rf "./#{analysis.attachments.first.bucket}"
+      end
+
+      it { expect(to_zip).to be_a ZipTricks::OutputEnumerator }
+      it { expect(Dir.glob("./tmp/output/*").count { |folder| File.directory?(folder) }).to eq 1 }
+      it { expect(BagIt::Bag.new(Dir.glob("./tmp/output/*").find { |folder| File.directory?(folder) }).valid?).to be true }
+      it { expect(Dir.new(Dir.glob("./tmp/output/*").find { |folder| File.directory?(folder) }).entries.size).to eq 9 }
+
+      it do
+        expect(File.size("#{analysis.attachments.first.bucket}/#{analysis.attachments.first.identifier}"))
+          .to eq File.size(Dir.glob("./tmp/output/*").find { |folder| File.directory?(folder) } + "/data/#{attachments.first.filename}")
+      end
+
+      it do
+        expect(File.size("#{analysis.attachments.first.bucket}/#{analysis.attachments.first.identifier}"))
+          .to eq File.size(Dir.glob("./tmp/output/*").find { |folder| File.directory?(folder) } + "/data/#{attachments.second.filename}")
+      end
+
+      it do
+        expect(File.size("#{analysis.attachments.first.bucket}/#{analysis.attachments.first.identifier}"))
+          .to eq File.size(Dir.glob("./tmp/output/*").find { |folder| File.directory?(folder) } + "/data/#{attachments[2].filename}")
+      end
+
+      it do
+        expect(File.size("#{analysis.attachments.first.bucket}/#{analysis.attachments.first.identifier}"))
+          .to eq File.size(Dir.glob("./tmp/output/*").find { |folder| File.directory?(folder) } + "/data/#{attachments[3].filename}")
+      end
+
+      it do
+        expect(File.size("#{analysis.attachments.first.bucket}/#{analysis.attachments.first.identifier}"))
+          .to eq File.size(Dir.glob("./tmp/output/*").find { |folder| File.directory?(folder) } + "/data/#{attachments[4].filename}")
       end
 
       it do
