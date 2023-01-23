a900e817d91adcb5b15b2372bbc99e69e11da449
Philipp Herda <philipp.herda@cleanercode.de>
2023-01-23 14:43:40 +0100

    WIP: implement streamable class for to_xml, to_json

--- app/presenters/concerns/has_to_json.rb
+++ app/presenters/concerns/has_to_json.rb
@@ -2,18 +2,36 @@ module HasToJson
   extend ActiveSupport::Concern
 
   def to_json
-    Enumerator.new do |yielder|
-      yielder << "["
-      publications.each.with_index(1) { |publication, index|
-        publication.present_to_api.to_json.each { |json_chunk|
-          yielder << ((index < publications_size) ? "#{json_chunk}," : json_chunk)
+    # Enumerator.new do |yielder|
+    #   yielder << "["
+    #   publications.each.with_index(1) { |publication, index|
+    #     publication.present_to_api.to_json.each { |json_chunk|
+    #       yielder << ((index < publications_size) ? "#{json_chunk}," : json_chunk)
+    #     }
+    #   }
+    #   yielder << "]"
+    # end.lazy
+
+    Class.new {
+      def initialize(publications) = @publications = publications
+
+      def each
+        yield "["
+
+        @publications.each.with_index(1) { |publication, index|
+          publication.present_to_api.to_json.each { |json_chunk|
+            yield ((index < publications_size) ? "#{json_chunk}," : json_chunk)
+          }
         }
-      }
-      yielder << "]"
-    end.lazy
+
+        yield "]"
+      end
+
+      def publications_size = @publications_size ||= @publications.size
+    }.new(publications)
   end
 
   private
 
-  def publications_size = @publications_size ||= publications.size
+  # def publications_size = @publications_size ||= publications.size
 end
--- app/presenters/concerns/has_to_xml.rb
+++ app/presenters/concerns/has_to_xml.rb
@@ -2,10 +2,26 @@ module HasToXml
   extend ActiveSupport::Concern
 
   def to_xml
-    Enumerator.new do |yielder|
-      yielder << "<publications>"
-      publications.each { |publication| publication.present_to_api.to_xml.each { |xml_chunk| yielder << xml_chunk } }
-      yielder << "</publications>"
-    end.lazy
+    # Enumerator.new do |yielder|
+    #   yielder << "<publications>"
+    #   publications.each { |publication| publication.present_to_api.to_xml.each { |xml_chunk| yielder << xml_chunk } }
+    #   yielder << "</publications>"
+    # end.lazy
+
+    # publications.map { |publication| publication.present_to_api.to_xml.to_a.join }
+
+    Class.new {
+      def initialize(publications) = @publications = publications
+
+      def each
+        yield "<publications>"
+
+        @publications.find_each do |publication|
+          publication.present_to_api.to_xml.each { |chunk| yield chunk }
+        end
+
+        yield "</publications>"
+      end
+    }.new(publications)
   end
 end
