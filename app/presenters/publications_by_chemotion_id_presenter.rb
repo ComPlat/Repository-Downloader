692f42dd88bbc95c34c12eb0fb8b443458720afd
Philipp Herda <philipp.herda@cleanercode.de>
2023-01-24 18:25:22 +0100

    Add HasToCsv

--- app/presenters/publications_by_chemotion_id_presenter.rb
+++ app/presenters/publications_by_chemotion_id_presenter.rb
@@ -2,6 +2,7 @@ class PublicationsByChemotionIdPresenter
   include HasToXml
   include HasToJson
   include HasToZip
+  include HasToCsv
 
   def initialize(chemotion_ids) = @chemotion_ids = chemotion_ids
 
