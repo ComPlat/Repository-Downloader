fef82c4069b1a1e5278b16f05ad662a62e4a0dde
Philipp Herda <philipp.herda@cleanercode.de>
2023-01-25 15:33:40 +0100

    Refactor csv_merger_spec.rb

--- spec/presenters/csv_merger_spec.rb
+++ spec/presenters/csv_merger_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 describe CsvMerger do
-  let(:reaction) { create(:reaction, :with_realistic_attributes, reaction_description: "reaction_description") } # HINT: Otherwise test is very complicated.
-  let(:sample) { create(:sample, :with_realistic_attributes, reaction:) }
   let(:csv_strings) {
+    reaction = create(:reaction, :with_realistic_attributes)
+    sample = create(:sample, :with_realistic_attributes, reaction:)
     {reaction_csv_string: reaction.present_to_api.to_csv.to_a.join,
      sample_csv_string: sample.present_to_api.to_csv.to_a.join}
   }
@@ -15,59 +15,63 @@ describe CsvMerger do
   describe "#merged_csv" do
     subject(:merged_csv_string) { described_class.new([csv_strings[:reaction_csv_string], csv_strings[:sample_csv_string]]).merged_csv_string }
 
-    let(:csv_helper_hash) {
-      {reaction_csv: CSV.parse(csv_strings[:reaction_csv_string], headers: true),
-       sample_csv: CSV.parse(csv_strings[:sample_csv_string], headers: true),
-       merged_csv_reaction_values: merged_csv_rows.second.map { |value| value.nil? ? "" : value },
-       merged_csv_sample_values: merged_csv_rows.third.map { |value| value.nil? ? "" : value }}
-    }
+    let(:expected_csv_table) {
+      reaction_csv = CSV.parse(csv_strings[:reaction_csv_string], headers: true)
+      reaction_csv_rows = reaction_csv.to_a
+      sample_csv = CSV.parse(csv_strings[:sample_csv_string], headers: true)
+      sample_csv_rows = sample_csv.to_a
+      merged_csv = CSV.parse(merged_csv_string, headers: true)
+      merged_csv_rows = merged_csv.to_a
 
-    let(:merged_csv) { CSV.parse(merged_csv_string, headers: true) }
-    let(:merged_csv_rows) { merged_csv.to_a }
+      nil_to_empty_string = ->(value) { value.nil? ? "" : value }
 
-    let(:csv_rows) {
-      {reaction_csv_rows: csv_helper_hash[:reaction_csv].to_a,
-       sample_csv_rows: csv_helper_hash[:sample_csv].to_a}
+      {reaction_csv:,
+       reaction_csv_rows:,
+       reaction_csv_values: reaction_csv_rows.second.map(&nil_to_empty_string),
+       sample_csv:,
+       sample_csv_rows:,
+       sample_csv_values: sample_csv_rows.second.map(&nil_to_empty_string),
+       merged_csv:,
+       merged_csv_headers: merged_csv.headers,
+       merged_csv_rows:,
+       merged_csv_sample_values: merged_csv_rows.third.map(&nil_to_empty_string),
+       merged_csv_reaction_values: merged_csv_rows.second.map(&nil_to_empty_string)}
     }
 
     it { is_expected.to be_a String }
 
     describe "rows" do
-      it { expect(merged_csv_rows).to all(all(be_a(String).or(be_nil))) }
-      it { expect(merged_csv_rows.size).to eq csv_rows[:reaction_csv_rows].size + csv_rows[:sample_csv_rows].size - 1 }
-      it { expect(merged_csv_rows.size).to eq 3 }
+      it { expect(expected_csv_table[:merged_csv_rows]).to all(all(be_a(String).or(be_nil))) }
+      it { expect(expected_csv_table[:merged_csv_rows].size).to eq expected_csv_table[:reaction_csv_rows].size + expected_csv_table[:sample_csv_rows].size - 1 }
+      it { expect(expected_csv_table[:merged_csv_rows].size).to eq 3 }
     end
 
     describe "headers" do
-      it { expect(merged_csv.headers).to all be_a String }
-      it { expect(merged_csv.headers).to include(*(csv_helper_hash[:reaction_csv].headers + csv_helper_hash[:sample_csv].headers)) }
-      it { expect(merged_csv.headers.size).to eq (csv_helper_hash[:reaction_csv].headers + csv_helper_hash[:sample_csv].headers).uniq.size }
-      it { expect(merged_csv.headers).to match_array((csv_helper_hash[:reaction_csv].headers + csv_helper_hash[:sample_csv].headers).uniq) }
+      it { expect(expected_csv_table[:merged_csv_headers]).to all be_a String }
+      it { expect(expected_csv_table[:merged_csv_headers]).to include(*(expected_csv_table[:reaction_csv].headers + expected_csv_table[:sample_csv].headers)) }
+      it { expect(expected_csv_table[:merged_csv_headers].size).to eq (expected_csv_table[:reaction_csv].headers + expected_csv_table[:sample_csv].headers).uniq.size }
+      it { expect(expected_csv_table[:merged_csv_headers]).to match_array((expected_csv_table[:reaction_csv].headers + expected_csv_table[:sample_csv].headers).uniq) }
     end
 
     describe "reaction row" do
-      let(:reaction_csv_values) { csv_rows[:reaction_csv_rows].second.map { |value| value.nil? ? "" : value } }
-
-      it { expect(csv_helper_hash[:merged_csv_reaction_values]).to all be_a String }
-      it { expect(csv_helper_hash[:merged_csv_reaction_values].size).to eq merged_csv.headers.size }
+      it { expect(expected_csv_table[:merged_csv_reaction_values]).to all be_a String }
+      it { expect(expected_csv_table[:merged_csv_reaction_values].size).to eq expected_csv_table[:merged_csv_headers].size }
 
       it {
-        expect(merged_csv.headers.zip(csv_helper_hash[:merged_csv_reaction_values]).to_h)
-          .to eq((merged_csv.headers - csv_helper_hash[:reaction_csv].headers).index_with { |header| "" }
-                   .merge(csv_helper_hash[:reaction_csv].headers.zip(reaction_csv_values).to_h))
+        expect(expected_csv_table[:merged_csv_headers].zip(expected_csv_table[:merged_csv_reaction_values]).to_h)
+          .to eq((expected_csv_table[:merged_csv_headers] - expected_csv_table[:reaction_csv].headers).index_with { |_header| "" }
+                   .merge(expected_csv_table[:reaction_csv].headers.zip(expected_csv_table[:reaction_csv_values]).to_h))
       }
     end
 
     describe "sample row" do
-      let(:sample_csv_values) { csv_rows[:sample_csv_rows].second.map { |value| value.nil? ? "" : value } }
-
-      it { expect(csv_helper_hash[:merged_csv_sample_values]).to all be_a String }
-      it { expect(csv_helper_hash[:merged_csv_sample_values].size).to eq merged_csv.headers.size }
+      it { expect(expected_csv_table[:merged_csv_sample_values]).to all be_a String }
+      it { expect(expected_csv_table[:merged_csv_sample_values].size).to eq expected_csv_table[:merged_csv_headers].size }
 
       it {
-        expect(merged_csv.headers.zip(csv_helper_hash[:merged_csv_sample_values]).to_h)
-          .to eq((merged_csv.headers - csv_helper_hash[:sample_csv].headers).index_with { |header| "" }
-                   .merge(csv_helper_hash[:sample_csv].headers.zip(sample_csv_values).to_h))
+        expect(expected_csv_table[:merged_csv_headers].zip(expected_csv_table[:merged_csv_sample_values]).to_h)
+          .to eq((expected_csv_table[:merged_csv_headers] - expected_csv_table[:sample_csv].headers).index_with { |_header| "" }
+                   .merge(expected_csv_table[:sample_csv].headers.zip(expected_csv_table[:sample_csv_values]).to_h))
       }
     end
   end
