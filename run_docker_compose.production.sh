5fddf87599f5d8185cc2211b840aa0e5e845800e
Tobias Vetter <tobias.vetter@mailbox.org>
2023-02-17 17:22:09 +0100

    adding easy way to provide ssh key for production environment and better naming

--- run_docker_compose.production.sh
+++ run_docker_compose.production.sh
@@ -1,9 +1,17 @@
+ssh_key=$(cat <<-END
+-----BEGIN OPENSSH PRIVATE KEY-----
+
+-----END OPENSSH PRIVATE KEY-----
+END
+)
+
 BAG_IT_STREAM_THREADS=5\
-  DOWNLOADER_DATA_PATH=/Users/philippherda/Pictures/\
+  DOWNLOADER_ATTACHMENTS_PATH=/Users/tbsvttr/Code/KIT/file_test\
   DOWNLOADER_DB_USERNAME=postgres\
   DOWNLOADER_DB_PASSWORD=postgres\
   DOWNLOADER_DB_HOST=host.docker.internal\
   DOWNLOADER_DB_PORT=5432\
   DOWNLOADER_DB_NAME=downloader_db_prod\
+  SSH_KEY=$ssh_key\
   docker compose -f docker-compose.production.yml up --remove-orphans --force-recreate --build
 # For debugging append to first line: --force-recreate --build
