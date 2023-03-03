bd083f06175bce5ed02fab431678e232327ecd83
Philipp Herda <philipp.herda@cleanercode.de>
2023-03-03 15:23:04 +0100

    Set secret_key_base to env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,9 +6,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  # Add key for testing purposes
-  # config.secret_key_base = "key_here"
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -90,4 +87,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
